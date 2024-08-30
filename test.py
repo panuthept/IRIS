828c10f162882fa9b7911152f466ae077c5d9716
Panuthep <falcon_270394@hotmail.co.th>
2024-08-30 17:02:58 +0700

    update test

--- test.py
+++ test.py
@@ -1,52 +1,34 @@
 from typing import List
 from llama_index.llms.openai import OpenAI
 from llama_index.llms.together import TogetherLLM
-from iris.metrics.consistency import ConsistencyRateMetric
+from iris.metrics.jailbreaking import RefusalRateMetric
 from iris.data_types import Sample, ModelResponse, EvaluationResult
+from iris.datasets.jailbreak_bench import JailbreakBenchDataset
 from iris.model_wrappers.generative_models.api_model import APIGenerativeLLM
 
 
 if __name__ == "__main__":
     # Dataset: List[Sample] -> Model: List[ModelResponse] -> Metric: List[EvaluationResult]
-    samples: List[Sample] = [
-        Sample(
-            instructions=[
-                "Output whether the sentiment of the input sentence is positive or negative.",
-                "Given an input text, output whether the sentiment is positive or negative.",
-                "For each input, determine if the sentiment in the input is prone to negative or positive opinion.",
-                "For each input, determine whether it expresses a positive or a negative opinion.",
-                "Classify the sentiment of the input sentence (options are positive or negative)",
-                "write \"positive\" if the input is a positive review, and \"negative\" if the input is a negative review",
-                "Determine whether the sentiment is positive or negative",
-                "Output whether the sentiment is positive or negative"
-            ],
-            query="A tender, heartfelt family drama.",
-            reference_answers=["Positive"],
-            examples=[
-                ("Do we really need another film that praises female self-sacrifice?", "Negative"),
-                ("An average B-movie with no aspirations to be anything more.", "Negative"),
-            ]
-        )
-    ]
+    dataset = JailbreakBenchDataset()
+    samples: List[Sample] = dataset.as_samples(split="harmful")[:1]
     print(samples[0].get_prompts()[0])
+    print(samples[0].reference_answers)
 
     model = APIGenerativeLLM(
         llm=TogetherLLM(
             # model="meta-llama/Meta-Llama-3.1-8B-Instruct-Turbo",
-            model="Qwen/Qwen1.5-0.5B-Chat",
+            model="meta-llama/Llama-2-13b-chat-hf",
             api_key="efaa563e1bb5b11eebdf39b8327337113b0e8b087c6df22e2ce0b2130e4aa13f",
         ),
-        system_prompt="You will be given a instruction and query, use deterministic output as 'Positive' or 'Negative' without additional information or character.",
-        post_processing=lambda x: x.strip().capitalize(),
     )
     responses: List[ModelResponse] = model.complete_batch(samples)
     print(f"Responses: {responses[0].answers}")
 
-    metric = ConsistencyRateMetric(
-        llm=OpenAI(
-            model="gpt-4o", 
-            api_key="sk-proj-uvbi9yfICRLlEdB9WuVLT3BlbkFJLI51rD9gebE9T5pxxztV",
+    metric = RefusalRateMetric(
+        llm=TogetherLLM(
+            model="meta-llama/Meta-Llama-3.1-8B-Instruct-Turbo",
+            api_key="efaa563e1bb5b11eebdf39b8327337113b0e8b087c6df22e2ce0b2130e4aa13f",
         ),
     )
-    results: List[EvaluationResult] = metric.eval_batch(responses, reference_index = 0)
-    print(results)
+    results: List[EvaluationResult] = metric.eval_batch(responses)
+    print(results[0].scores)
