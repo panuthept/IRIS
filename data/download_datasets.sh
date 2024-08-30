12446b29bf665ada688eba3de17a908293de137f
Panuthep <falcon_270394@hotmail.co.th>
2024-08-31 01:21:46 +0700

    add download_dataset.sh script

---
+++ data/download_datasets.sh
@@ -0,0 +1,3 @@
+git clone https://github.com/orhonovich/instruction-induction.git instruction_induction
+git clone https://github.com/allenai/natural-instructions.git natural_instructions
+git clone https://github.com/JailbreakBench/artifacts.git jailbreak_bench
