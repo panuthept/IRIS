e26cea861c0c6867cd6818dd3c46ff3c327a8d0d
Panuthep <falcon_270394@hotmail.co.th>
2024-08-31 16:17:59 +0700

    add download_model.py

---
+++ download_model.py
@@ -0,0 +1,24 @@
+import torch
+import argparse
+from iris.model_wrappers.generative_models.huggingface_model import HuggfaceGenerativeLLM
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--model_name", type=str, default="Qwen/Qwen2-0.5B-Instruct")
+    args = parser.parse_args()
+
+    model = HuggfaceGenerativeLLM(
+        args.model_name,
+        max_tokens=512,
+        pipeline_kwargs={
+            "torch_dtype": torch.bfloat16,
+            "model_kwargs": {
+                "cache_dir": "./models",
+                "local_files_only": False,
+            }
+        },
+        cache_path="./cache",
+    )
+    print(model)
+    print(f"Downloaded model: {model} successfully!")
