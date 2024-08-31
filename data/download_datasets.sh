66ec7244762c7565c5d327c26414398d543bec99
Panuthep <falcon_270394@hotmail.co.th>
2024-08-31 17:20:02 +0700

    update download_datasets script

--- data/download_datasets.sh
+++ data/download_datasets.sh
@@ -1,3 +1,8 @@
 git clone https://github.com/orhonovich/instruction-induction.git instruction_induction
 git clone https://github.com/allenai/natural-instructions.git natural_instructions
 git clone https://github.com/JailbreakBench/artifacts.git jailbreak_bench
+
+mkdir datasets
+mv instruction_induction datasets/instruction_induction
+mv natural_instructions datasets/natural_instructions
+mv jailbreak_bench datasets/jailbreak_bench
