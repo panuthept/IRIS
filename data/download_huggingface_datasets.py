312fe7a8e42752f48df4d97c0eeb7c011053336c
Panuthep <falcon_270394@hotmail.co.th>
2024-09-09 22:32:37 +0700

    Add download dataset script for wildguardmix

--- data/download_huggingface_datasets.py
+++ data/download_huggingface_datasets.py
@@ -3,3 +3,6 @@ from datasets import load_dataset
 
 load_dataset("JailbreakBench/JBB-Behaviors", "behaviors", cache_dir="./datasets/jailbreak_bench")
 load_dataset("JailbreakBench/JBB-Behaviors", "judge_comparison", cache_dir="./datasets/jailbreak_bench")
+
+load_dataset("allenai/wildguardmix", "wildguardtrain", cache_dir="./data/datasets/wildguardmix")
+load_dataset("allenai/wildguardmix", "wildguardtest", cache_dir="./data/datasets/wildguardmix")
