d5fc448028c58cd751a6219d062e8bfe4fed1215
KornWtp <elecwpr@gmail.com>
2024-09-09 14:17:15 +0700

    Add promptsource datasets

--- data/download_datasets.sh
+++ data/download_datasets.sh
@@ -3,6 +3,7 @@ git clone https://github.com/allenai/natural-instructions.git natural_instructio
 git clone https://github.com/JailbreakBench/artifacts.git jailbreak_bench
 git clone https://github.com/EddyLuo1232/JailBreakV_28K.git jailbreakv_28k
 git clone https://github.com/paul-rottger/exaggerated-safety.git xstest
+git clone https://github.com/bigscience-workshop/promptsource.git promptsource
 
 mkdir datasets
 mv instruction_induction datasets/instruction_induction
@@ -10,5 +11,6 @@ mv natural_instructions datasets/natural_instructions
 mv jailbreak_bench datasets/jailbreak_bench
 mv jailbreakv_28k datasets/jailbreakv_28k
 mv xstest datasets/xstest
+mv promptsource datasets/promptsource
 
 python download_huggingface_datasets.py
