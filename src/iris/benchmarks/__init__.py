5d00fe473dc135527dcea4a502bdc793a79d4db1
Panuthep <falcon_270394@hotmail.co.th>
2024-09-09 22:08:51 +0700

    resolve conflict

--- src/iris/benchmarks/__init__.py
+++ src/iris/benchmarks/__init__.py
@@ -1,7 +1,7 @@
 from iris.benchmarks.xstest import XSTestBenchmark
 from iris.benchmarks.jailbreakv_28k import JailbreaKV28kBenchmark
-from iris.benchmarks.instruction_induction import InstructionIndutionBenchmark
 from iris.benchmarks.promptsource_bench import PromptSourceBenchmark
+from iris.benchmarks.instruction_induction import InstructionIndutionBenchmark
 from iris.benchmarks.base import Benchmark, JailbreakBenchmark, JailbreakPromptCLFBenchmark
 from iris.benchmarks.wildguardmix import WildGuardMixBenchmark, WildGuardMixPromptCLFBenchmark
 from iris.benchmarks.jailbreak_bench import JailbreakBenchBenchmark, JailbreakBenchPromptCLFBenchmark
