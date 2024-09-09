f30628ddea008acff51491e268cc35996202f1dc
Panuthep <falcon_270394@hotmail.co.th>
2024-09-09 21:32:38 +0700

    Support apply_chat_template=False

--- src/iris/model_wrappers/generative_models/api_model.py
+++ src/iris/model_wrappers/generative_models/api_model.py
@@ -15,13 +15,18 @@ class APIGenerativeLLM(GenerativeLLM):
     def get_model_name(self) -> str:
         return self.llm.model
 
-    def _complete(self, promt: str, ref_prompt: str = None, **kwargs) -> str:
-        if self.system_prompt:
-            messages = [
-                ChatMessage(role="system", content=self.system_prompt),
-                ChatMessage(role="user", content=promt),
-            ]
+    def _complete(self, prompt: str, ref_prompt: str = None, apply_chat_template: bool = True, **kwargs) -> str:
+        if apply_chat_template:
+            if self.system_prompt:
+                messages = [
+                    ChatMessage(role="system", content=self.system_prompt),
+                    ChatMessage(role="user", content=prompt),
+                ]
+            else:
+                messages = [ChatMessage(role="user", content=prompt)]
+            answer = self.llm.chat(messages, **kwargs).message.content
         else:
-            messages = [ChatMessage(role="user", content=promt)]
-        answer = self.llm.chat(messages, **kwargs).message.content
+            if self.system_prompt:
+                prompt = f"{self.system_prompt}\n\n{prompt}"
+            answer = self.llm.complete(prompt, **kwargs)
         return answer
