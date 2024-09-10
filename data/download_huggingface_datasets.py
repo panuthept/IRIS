776a50aa75b7adfafdf7e0758f45ebe17aacc918
Panuthep <falcon_270394@hotmail.co.th>
2024-09-10 21:39:39 +0700

    Update download script

--- data/download_huggingface_datasets.py
+++ data/download_huggingface_datasets.py
@@ -1,8 +1,15 @@
+import os
 from datasets import load_dataset
 
 
-load_dataset("JailbreakBench/JBB-Behaviors", "behaviors", cache_dir="./datasets/jailbreak_bench")
-load_dataset("JailbreakBench/JBB-Behaviors", "judge_comparison", cache_dir="./datasets/jailbreak_bench")
+if os.path.exists("./datasets") is False:
+    os.mkdir("./datasets")
 
-load_dataset("allenai/wildguardmix", "wildguardtrain", cache_dir="./datasets/wildguardmix")
-load_dataset("allenai/wildguardmix", "wildguardtest", cache_dir="./datasets/wildguardmix")
+if os.path.exists("./datasets/jailbreak_bench") is False:
+    os.mkdir("./datasets/jailbreak_bench")
+    load_dataset("JailbreakBench/JBB-Behaviors", "behaviors", cache_dir="./datasets/jailbreak_bench")
+    load_dataset("JailbreakBench/JBB-Behaviors", "judge_comparison", cache_dir="./datasets/jailbreak_bench")
+
+if os.path.exists("./datasets/wildguardmix") is False:
+    load_dataset("allenai/wildguardmix", "wildguardtrain", cache_dir="./datasets/wildguardmix")
+    load_dataset("allenai/wildguardmix", "wildguardtest", cache_dir="./datasets/wildguardmix")
