cbedbfddaa19a17f3a1bdaf79e905d86083ea505
Panuthep <falcon_270394@hotmail.co.th>
2024-09-10 21:36:35 +0700

    Update download script

--- data/download_huggingface_datasets.py
+++ data/download_huggingface_datasets.py
@@ -4,5 +4,5 @@ from datasets import load_dataset
 load_dataset("JailbreakBench/JBB-Behaviors", "behaviors", cache_dir="./datasets/jailbreak_bench")
 load_dataset("JailbreakBench/JBB-Behaviors", "judge_comparison", cache_dir="./datasets/jailbreak_bench")
 
-load_dataset("allenai/wildguardmix", "wildguardtrain", cache_dir="./data/datasets/wildguardmix")
-load_dataset("allenai/wildguardmix", "wildguardtest", cache_dir="./data/datasets/wildguardmix")
+load_dataset("allenai/wildguardmix", "wildguardtrain", cache_dir="./datasets/wildguardmix")
+load_dataset("allenai/wildguardmix", "wildguardtest", cache_dir="./datasets/wildguardmix")
