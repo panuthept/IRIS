407c5c1d54fe1d968fe0a43cdd39420df96c14fd
Panuthep <falcon_270394@hotmail.co.th>
2024-09-12 23:13:40 +0700

    Add progress bar to InstructionAugmentation

--- src/iris/augmentations/instruction_augmentations/jailbreaks/base.py
+++ src/iris/augmentations/instruction_augmentations/jailbreaks/base.py
@@ -1,3 +1,5 @@
+from typing import List
+from iris.data_types import Sample
 from iris.augmentations.instruction_augmentations import InstructionAugmentation
 
 
@@ -7,3 +9,13 @@ class Jailbreaking(InstructionAugmentation):
         include_failed_cases: bool = False,
     ):
         self.include_failed_cases = include_failed_cases
+        self.attack_success_rate = None
+
+    def augment_batch(self, samples: List[Sample], verbose: bool = True) -> List[Sample]:
+        samples = super().augment_batch(samples, verbose=verbose)
+
+        attack_success_count = 0
+        for sample in samples:
+            attack_success_count += int(len(sample.instructions) > 0)
+        self.attack_success_rate = attack_success_count / len(samples)
+        return samples
