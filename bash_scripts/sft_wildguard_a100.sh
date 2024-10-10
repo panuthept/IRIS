a78bb70091a117a022413c3aba663975870d6ca1
Panuthep <falcon_270394@hotmail.co.th>
2024-10-10 19:10:14 +0700

    Add train script

---
+++ bash_scripts/sft_wildguard_a100.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -l
+
+#SBATCH --job-name=SFT_WildGuard_A100
+#SBATCH --output=sft_wildguard_A100.out
+#SBATCH --nodes=1
+#SBATCH --partition=scads-a100
+#SBATCH --account=scads
+#SBATCH --gres=gpu:1
+#SBATCH --mem=128gb
+#SBATCH --time=5-00:00:0
+#SBATCH --cpus-per-task=10
+
+port=$(shuf -i 6000-9999 -n 1)
+USER=$(whoami)
+node=$(hostname -s)
+
+unset XDG_RUNTIME_DIR
+if [ "$SLURM_JOBTMP" != "" ]; then
+    export XDG_RUNTIME_DIR=$SLURM_JOBTMP
+fi
+
+CUDA_LAUNCH_BLOCKING=1 ~/.conda/envs/iris/bin/python scripts/sft_wildguard.py \
+--model_name facebook/opt-350m
