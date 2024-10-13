59e21835edd038be27377ec9102b2bc4fdea4d90
Panuthep <falcon_270394@hotmail.co.th>
2024-10-13 17:42:26 +0700

    Add sft_wildguard_adversarial_a100.sh and sft_wildguard_vanilla_a100.sh

---
+++ bash_scripts/sft_wildguard_adversarial_a100.sh
@@ -0,0 +1,29 @@
+#!/bin/bash -l
+
+#SBATCH --job-name=SFT_WildGuard_Adversarial_A100
+#SBATCH --output=sft_wildguard_Adversarial_A100.out
+#SBATCH --nodes=1
+#SBATCH --partition=scads-a100
+#SBATCH --account=scads
+#SBATCH --gres=gpu:1
+#SBATCH --mem=128gb
+#SBATCH --time=5-00:00:0
+#SBATCH --cpus-per-task=10
+
+port=$(shuf -i 6000-9999 -n 1)
+USER=$(whoami)
+node=$(hostname -s)
+
+unset XDG_RUNTIME_DIR
+if [ "$SLURM_JOBTMP" != "" ]; then
+    export XDG_RUNTIME_DIR=$SLURM_JOBTMP
+fi
+
+CUDA_LAUNCH_BLOCKING=1 ~/.conda/envs/iris/bin/python scripts/sft_wildguard.py \
+--model_name facebook/opt-350m \
+--attack_engine adversarial \
+--max_seq_length 2048 \
+--batch_size 1 \
+--eval_steps 60 \
+--gradient_accumulation_steps 128 \
+--output_dir ./finetuned_models/sft_wildguard_adversarial_opt_350m
