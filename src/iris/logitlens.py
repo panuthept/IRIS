825506a832a18a4e3562016f4571a2f2931f5348
Panuthep <falcon_270394@hotmail.co.th>
2024-10-16 20:45:02 +0700

    Update LogitLens

--- src/iris/logitlens.py
+++ src/iris/logitlens.py
@@ -22,41 +22,59 @@ class LogitLens:
         self.k = k
         self.lm_head = lm_head
         self.tokenizer = tokenizer
-        self.module_names = self.resolve_module_names(module_names)
+        self.module_names = self._resolve_module_names(module_names)
         self.cached_activations: Dict[str, List[List[int]]] = {}
 
-    def resolve_module_names(self, module_names):
+    def clear_cache(self):
+        self.cached_activations: Dict[str, List[List[int]]] = {}
+
+    def _resolve_module_names(self, module_names):
         if module_names is None:
             return []
         if isinstance(module_names, str):
             return DEFAULT_MODULE_NAMES.get(module_names, [])
         return module_names
 
-    def register_hooks(self, model):
-        for name, module in model.named_modules():
-            if name in self.module_names:
-                module.register_forward_hook(self.add_hook(name))
-
-    def extract_activations(self, activations: Union[Tuple[Tensor], Tensor]) -> List[List[int]]:
+    def _extract_activations(self, activations: Union[Tuple[Tensor], Tensor]) -> List[List[int]]:
         if isinstance(activations, tuple):
             activations = activations[0]
         logits = self.lm_head(activations[:, -1])
         return [torch.argsort(logits[batch_idx], descending=True)[:self.k].tolist() for batch_idx in range(logits.size(0))]
 
-    def add_hook(self, name):
+    def _add_hook(self, name):
         def hook(model, input, output):
             if name not in self.cached_activations:
                 self.cached_activations[name] = []
-            self.cached_activations[name].extend(self.extract_activations(output))
+            self.cached_activations[name].extend(self._extract_activations(output))
         return hook
+    
+    def register_hooks(self, model):
+        for name, module in model.named_modules():
+            if name in self.module_names:
+                module.register_forward_hook(self._add_hook(name))
     
     def cache_logits(self, logits: Tensor, module_name: str):
         if module_name not in self.cached_activations:
             self.cached_activations[module_name] = []
         self.cached_activations[module_name].extend([torch.argsort(logits[batch_idx], descending=True)[:self.k].tolist() for batch_idx in range(logits.size(0))])
 
-    def get_last_activations(self) -> Dict[str, List[int]]:
+    def get_last_activations(self) -> Dict[str, List[str]]:
         last_activations = {}
         for module_name in self.cached_activations:
             last_activations[module_name] = self.tokenizer.convert_ids_to_tokens(self.cached_activations[module_name][-1])
         return last_activations
+    
+    def get_activations(self) -> Dict[str, List[Tuple[str, int]]]:
+        # Accumulate activations
+        accum_activations = {}
+        for module_name in self.cached_activations:
+            accum_activations[module_name] = {}
+            for sample_activations in self.cached_activations[module_name]:
+                for token_id in sample_activations:
+                    token = self.tokenizer.convert_ids_to_tokens([token_id])[0]
+                    if token not in accum_activations[module_name]:
+                        accum_activations[module_name][token] = 0
+                    accum_activations[module_name][token] += 1
+            # Sort activations
+            accum_activations[module_name] = sorted(accum_activations[module_name].items(), key=lambda x: x[1], reverse=True)[:self.k]
+        return accum_activations
