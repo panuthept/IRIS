a73eb34c4767be7c93867e593a9b118ba09bfbf3
Panuthep <falcon_270394@hotmail.co.th>
2024-10-17 20:32:38 +0700

    Update LogitLens

--- src/iris/logitlens.py
+++ src/iris/logitlens.py
@@ -1,7 +1,7 @@
 import torch
 from torch import nn, Tensor
 from transformers import AutoTokenizer
-from typing import List, Dict, Union, Tuple
+from typing import List, Dict, Any, Union, Tuple
 
 
 DEFAULT_MODULE_NAMES = {
@@ -20,16 +20,19 @@ class LogitLens:
         lm_head: nn.Module,
         tokenizer: AutoTokenizer,
         module_names: Union[List[str], str] = None,
-        k: int = 5,
+        k: int = 50,
     ):
         self.k = k
         self.lm_head = lm_head
         self.tokenizer = tokenizer
         self.module_names = self._resolve_module_names(module_names)
-        self.cached_activations: Dict[str, List[List[int]]] = {}
 
-    def clear_cache(self):
-        self.cached_activations: Dict[str, List[List[int]]] = {}
+        self.cached_activations: Dict[str, Tensor] = {}
+        self.cached_logits: Dict[str, List[List[Tuple[int, float]]]] = {}
+
+    def _clear_cache(self):
+        self.cached_activations: Dict[str, Tensor] = {}
+        self.cached_logits: Dict[str, List[List[Tuple[int, float]]]] = {}
 
     def _resolve_module_names(self, module_names):
         if module_names is None:
@@ -38,18 +41,37 @@ class LogitLens:
             return DEFAULT_MODULE_NAMES.get(module_names, [])
         return module_names
 
-    def _extract_activations(self, activations: Union[Tuple[Tensor], Tensor]) -> List[List[int]]:
-        if isinstance(activations, tuple):
-            activations = activations[0]
-        logits = self.lm_head(activations[:, -1])
-        return [torch.argsort(logits[batch_idx], descending=True)[:self.k].tolist() for batch_idx in range(logits.size(0))]
-
     def _add_hook(self, name):
         def hook(model, input, output):
+            # Output of a self-attention layer is a tuple
+            if isinstance(output, tuple):
+                output = output[0]
+            # Get last token activations
+            activations = output[:, -1]  # (batch_size, hidden_size)
+            logits = self.lm_head(activations)  # (batch_size, vocab_size)
+            token_ids: List[List[int]] = [torch.argsort(logits[batch_idx], descending=True)[:self.k].tolist() for batch_idx in range(logits.size(0))]
+
+            # Update cached_activations
+            activations = activations.detach().cpu().clone()
             if name not in self.cached_activations:
-                self.cached_activations[name] = []
-            self.cached_activations[name].extend(self._extract_activations(output))
+                self.cached_activations[name] = activations
+            else:
+                self.cached_activations[name] = torch.cat([self.cached_activations[name], activations], dim=0)
+            
+            # Update cached_logits
+            logits = logits.detach().cpu().clone()
+            if name not in self.cached_logits:
+                self.cached_logits[name] = []
+            self.cached_logits[name].extend(list(zip(token_ids, logits)))
         return hook
+    
+    def _decode(self, logits: List[List[Tuple[int, float]]]) -> List[List[Tuple[str, float]]]:
+        tokens: List[List[Tuple[str, float]]] = []
+        for sample_logits in logits:
+            tokens = self.tokenizer.convert_ids_to_tokens([token_id for token_id, _ in sample_logits])
+            scores = [score for _, score in sample_logits]
+            tokens.append(list(zip(tokens, scores)))
+        return tokens
     
     def register_hooks(self, model):
         for name, module in model.named_modules():
@@ -57,28 +79,40 @@ class LogitLens:
                 module.register_forward_hook(self._add_hook(name))
     
     def cache_logits(self, logits: Tensor, module_name: str):
-        if module_name not in self.cached_activations:
-            self.cached_activations[module_name] = []
-        self.cached_activations[module_name].extend([torch.argsort(logits[batch_idx], descending=True)[:self.k].tolist() for batch_idx in range(logits.size(0))])
+        token_ids: List[List[int]] = [torch.argsort(logits[batch_idx], descending=True)[:self.k].tolist() for batch_idx in range(logits.size(0))]
+        # Update cached_logits
+        logits = logits.detach().cpu().clone()
+        if module_name not in self.cached_logits:
+            self.cached_logits[module_name] = []
+        self.cached_logits[module_name].extend(list(zip(token_ids, logits)))
 
-    def get_last_activations(
-        self, 
-        decode_activations: bool = True,
-    ) -> Dict[str, Union[List[str], Tensor]]:
-        last_activations = {}
-        for module_name in self.cached_activations:
-            activations = self.cached_activations[module_name][-1]
-            if decode_activations:
-                activations = self.tokenizer.convert_ids_to_tokens(activations)
-            last_activations[module_name] = activations
-        return last_activations
+    def fetch_cache(self, decode: bool = True) -> Dict[str, Any]:
+        cache = {
+            "tokens": self._decode(self.cached_logits) if decode else None, 
+            "logits": self.cached_logits, 
+            "activations": self.cached_activations
+        }
+        self._clear_cache()
+        return cache
+
+    # def get_last_activations(
+    #     self, 
+    #     decode_activations: bool = True,
+    # ) -> Dict[str, Union[List[str], Tensor]]:
+    #     last_activations = {}
+    #     for module_name in self.cached_logits:
+    #         activations = self.cached_logits[module_name][-1]
+    #         if decode_activations:
+    #             activations = self.tokenizer.convert_ids_to_tokens(activations)
+    #         last_activations[module_name] = activations
+    #     return last_activations
     
     # def get_activations(self, decode_activations: bool = True) -> Dict[str, Union[List[Tuple[str, int]], Tensor]]:
     #     # Accumulate activations
     #     accum_activations = {}
-    #     for module_name in self.cached_activations:
+    #     for module_name in self.cached_logits:
     #         accum_activations[module_name] = {}
-    #         for sample_activations in self.cached_activations[module_name]:
+    #         for sample_activations in self.cached_logits[module_name]:
     #             for token_id in sample_activations:
     #                 token = self.tokenizer.convert_ids_to_tokens([token_id])[0]
     #                 if token not in accum_activations[module_name]:
