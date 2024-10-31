e2992d883862a1d58eeaada86f469a58d4556deb
Panuthep <falcon_270394@hotmail.co.th>
2024-10-31 21:04:49 +0700

    update inference script

--- bash_scripts/inference_wildguard.sh
+++ bash_scripts/inference_wildguard.sh
@@ -1,4 +1,4 @@
-for model_name in "iris_wildguard_layer_17" "iris_wildguard_layer_18" "iris_wildguard_layer_19" "iris_wildguard_layer_19_r2" "iris_wildguard_layer_19_negative" "iris_wildguard_layer_19_rand_1" "iris_wildguard_layer_19_rand_2" "iris_wildguard_layer_19_rand_3" "iris_wildguard_layer_19_rand_4" "iris_wildguard_layer_19_smooth_001" "iris_wildguard_layer_19_smooth_005" "iris_wildguard_layer_19_smooth_01" "iris_wildguard_layer_19_smooth_02" "iris_wildguard_layer_20" "iris_wildguard_layer_21" "iris_wildguard_layer_22" "iris_wildguard_layer_23" "iris_wildguard_layer_24" "iris_wildguard_layer_25" "iris_wildguard_layer_26" "iris_wildguard_layer_27"
+for model_name in "iris_wildguard_layer_19_freeze"
 do
 echo "Inference for ${model_name} on JailbreakBenchDataset"
 CUDA_VISIBLE_DEVICES=0 python scripts/inference_wildguard.py \
