c4f50c017caf0c156e87d06b27268442c95c6a03
Panuthep <falcon_270394@hotmail.co.th>
2024-11-18 22:27:43 +0700

    Add layer_search.py

---
+++ scripts/layer_search.py
@@ -0,0 +1,57 @@
+import os
+import json
+import random
+import argparse
+from tqdm import tqdm
+from iris.datasets import WildGuardMixDataset
+from iris.model_wrappers.guard_models import WildGuard
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--model_name", type=str, default="allenai/wildguard")
+    parser.add_argument("--checkpoint_path", type=str, default=None)
+    parser.add_argument("--train_eval_split", type=float, default=0.9)
+    parser.add_argument("--seed", type=int, default=42)
+    parser.add_argument("--save_path", type=str, default="./layer_search_outputs/wildguard/responses.jsonl")
+    args = parser.parse_args()
+
+    random.seed(args.seed)
+
+    # Load model
+    model = WildGuard(
+        model_name_or_path=args.model_name,
+        checkpoint_path=args.checkpoint_path,
+    )
+    
+    # Load dataset
+    dataset = WildGuardMixDataset(attack_engine=args.attack_engine, cache_dir=args.cache_dir)
+    samples = dataset.as_samples(split="train")
+
+    # Get development set
+    random.shuffle(samples)
+    if args.train_eval_split == 1.0:
+        train_samples, eval_samples = samples, []
+    else:
+        train_size = int(len(samples) * args.train_eval_split)
+        train_samples, eval_samples = samples[:train_size], samples[train_size:]
+    print(f"Dev size: {len(eval_samples)}")
+
+    if os.path.exists(args.save_path):
+        with open(args.save_path, "w") as f:
+            for sample in tqdm(samples):
+                prompts = sample.get_prompts()
+                gold_labels = sample.instructions_true_label
+                for prompt, gold_label in zip(prompts, gold_labels):
+                    response = model.generate(prompt, return_probs=True)
+                    cache = model.model.logitlens.fetch_cache(
+                        return_tokens=False, 
+                        return_logits=True, 
+                        return_activations=True,
+                    )
+                    f.write(json.dumps({
+                        "prompt": prompt,
+                        "response": response,
+                        "label": gold_label,
+                        "cache": cache
+                    }, ensure_ascii=False) + "\n")
