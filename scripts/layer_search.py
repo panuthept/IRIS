a95cf3acbb520603de6a30c5731555c33c394bdf
Panuthep <falcon_270394@hotmail.co.th>
2024-11-19 10:55:48 +0700

    Fix bug

--- scripts/layer_search.py
+++ scripts/layer_search.py
@@ -2,7 +2,9 @@ import os
 import json
 import random
 import argparse
+import numpy as np
 from tqdm import tqdm
+import matplotlib.pyplot as plt
 from iris.datasets import WildGuardMixDataset
 from iris.model_wrappers.guard_models import WildGuard
 
@@ -32,17 +34,27 @@ CUDA_VISIBLE_DEVICES=1 python scripts/layer_search.py \
 """
 
 
+def distance_function(activations, centroids, distance_function):
+    if distance_function == "L2":
+        return np.linalg.norm(activations - centroids)
+    elif distance_function == "Dot":
+        return np.dot(activations, centroids)
+    elif distance_function == "Cosine":
+        return np.dot(activations, centroids) / (np.linalg.norm(activations) * np.linalg.norm(centroids))
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--model_name", type=str, default="allenai/wildguard")
     parser.add_argument("--checkpoint_path", type=str, default=None)
     parser.add_argument("--train_eval_split", type=float, default=0.9)
     parser.add_argument("--seed", type=int, default=42)
+    parser.add_argument("--distance_function", type=str, default="L2", choices=["L2", "Dot", "Cosine"])
     parser.add_argument("--max_benign", type=int, default=1000)
     parser.add_argument("--max_harmful", type=int, default=1000)
     parser.add_argument("--save_logits", action="store_true")
     parser.add_argument("--save_activations", action="store_true")
-    parser.add_argument("--save_path", type=str, default="./layer_search_outputs/wildguard/responses.jsonl")
+    parser.add_argument("--save_path", type=str, default="./activations.jsonl")
     args = parser.parse_args()
 
     if not os.path.exists(args.save_path):
@@ -94,7 +106,7 @@ if __name__ == "__main__":
                         return_activations=args.save_activations,
                     )
                     # Remove self_attn and mlp
-                    cache = {k: {module_name: activation for module_name, activation in v.items() if "self_attn" in module_name or "mlp" in module_name} for k, v in cache.items()}
+                    cache = {k: {module_name: activation for module_name, activation in v.items() if "self_attn" not in module_name and "mlp" not in module_name} for k, v in cache.items()}
                     f.write(json.dumps({
                         "prompt": prompt,
                         "response": response,
@@ -108,6 +120,49 @@ if __name__ == "__main__":
 
     responses = []
     with open(args.save_path, "r") as f:
-        for line in open(args.save_path):
-            responses.append(json.loads(line))
-    print(responses[0]["cache"]["activations"].keys())
+        for line in tqdm(open(args.save_path)):
+            data = json.loads(line)
+            responses.append(data)
+    layer_names = list(responses[0]["cache"]["activations"].keys())
+    print(layer_names)
+
+    # Get centroids
+    centroids = {
+        "Harmful": {layer_name: [] for layer_name in layer_names}, 
+        "Benign": {layer_name: [] for layer_name in layer_names},
+    }
+    for layer_name in layer_names:
+        for response in responses:
+            label = response["label"]
+            activation = response["cache"]["activations"][layer_name][0]
+            centroids[label][layer_name].append(activation)
+        centroids[label][layer_name] = np.array(centroids[label][layer_name]).mean(axis=0)
+
+    # Get variations
+    variations = {
+        "Interclass": {layer_name: [] for layer_name in layer_names},
+        "Intraclass": {layer_name: [] for layer_name in layer_names},
+    }
+    for layer_name in layer_names:
+        for reseponse in responses:
+            label = response["label"]
+            activation = response["cache"]["activations"][layer_name][0]
+            distance = distance_function(activation, centroids[label][layer_name], args.distance_function)
+            for centroid_label in centroids.keys():
+                if label == centroid_label:
+                    variations["Intraclass"][layer_name].append(distance)
+                else:
+                    variations["Interclass"][layer_name].append(distance)
+        variations["Intraclass"][layer_name] = np.array(variations["Intraclass"][layer_name])
+        variations["Interclass"][layer_name] = np.array(variations["Interclass"][layer_name])
+
+    # Plot (x-axis: layer_name, y-axis: variations)
+    for class_name, class_variation in variations.items():
+        xs = list(range(len(class_variation)))
+        ys = [layer_variation for layer_variation in class_variation.values()]
+        plt.plot(xs, ys, label=class_name)
+        plt.xticks(xs, list(class_variation.keys()), rotation=90)
+        plt.xlabel("Layer")
+        plt.ylabel("Inter/Intra Variations")
+        plt.legend()
+    plt.show()
