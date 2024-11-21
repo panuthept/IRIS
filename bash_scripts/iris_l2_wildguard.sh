9655c8239b818b9aa5ed49a46e5c5aa511ae6cce
Panuthep <falcon_270394@hotmail.co.th>
2024-11-21 10:58:27 +0700

    Add training script

---
+++ bash_scripts/iris_l2_wildguard.sh
@@ -0,0 +1,29 @@
+echo "Running WildGuard-IRISL2 on layer 19 (Benign only)"
+CUDA_VISIBLE_DEVICES=0,1,2,3 accelerate launch --num_processes=4 scripts/iris_l2_wildguard.py \
+--iris_config ./data/iris_l2_configs/benign_only_configs/layer_19.json \
+--model_name allenai/wildguard \
+--train_eval_split 0.9 \
+--max_seq_length 2048 \
+--batch_size 1 \
+--gradient_accumulation_steps 32 \
+--epochs 2 \
+--eval_steps 60 \
+--save_total_limit 100 \
+--output_dir ./finetuned_models/iris_l2_wildguard_layer_19_benign_only \
+--use_lora \
+--lora_rank 128
+
+echo "Running WildGuard-IRISL2 on layer 19"
+CUDA_VISIBLE_DEVICES=0,1,2,3 accelerate launch --num_processes=4 scripts/iris_l2_wildguard.py \
+--iris_config ./data/iris_l2_configs/layer_19.json \
+--model_name allenai/wildguard \
+--train_eval_split 0.9 \
+--max_seq_length 2048 \
+--batch_size 1 \
+--gradient_accumulation_steps 32 \
+--epochs 2 \
+--eval_steps 60 \
+--save_total_limit 100 \
+--output_dir ./finetuned_models/iris_l2_wildguard_layer_19 \
+--use_lora \
+--lora_rank 128
