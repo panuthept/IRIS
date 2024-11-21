9b66725230dbabe01444214456356b3727fe6a82
Panuthep <falcon_270394@hotmail.co.th>
2024-11-21 14:52:41 +0700

    Update eval_record

--- scripts/eval_record.py
+++ scripts/eval_record.py
@@ -4,6 +4,16 @@ from tqdm import tqdm
 from iris.metrics.safeguard_metrics import SafeGuardMetric
 
 
+"""
+python scripts/eval_record.py \
+--load_path_a1 ./outputs/wildguard/ORBenchDataset/test/hard_benign_prompts.jsonl \
+--load_path_a2 ./outputs/wildguard/ORBenchDataset/test/harmful_prompts.jsonl \
+--load_path_b1 ./outputs/iris_wildguard_layer_19/ORBenchDataset/test/hard_benign_prompts.jsonl \
+--load_path_b2 ./outputs/iris_wildguard_layer_19/ORBenchDataset/test/harmful_prompts.jsonl \
+--dataset_name ORBench
+"""
+
+
 def load_examples(load_path: str):
     gold_labels = []
     pred_scores = []
@@ -41,11 +51,6 @@ if __name__ == "__main__":
     metrics_a = SafeGuardMetric()
     metrics_a.update(all_gold_labels, all_pred_scores)
 
-    # best_index = np.argmax(metrics_a.f1s)
-    # best_f1_a = metrics_a.f1s[best_index]
-    # best_recall_a = metrics_a.recalls[best_index]
-    # best_precision_a = metrics_a.precisions[best_index]
-
     all_gold_labels, all_pred_scores = load_examples(args.load_path_b1)
     if args.load_path_b2:
         gold_labels, pred_scores = load_examples(args.load_path_b2)
@@ -60,37 +65,3 @@ if __name__ == "__main__":
     metrics_b.update(all_gold_labels, all_pred_scores)
 
     metrics_b.plot_comparison(metrics_a, "WildGuard", args.dataset_name)
-
-    # best_index = np.argmax(metrics_b.f1s)
-    # best_f1_b = metrics_b.f1s[best_index]
-    # best_recall_b = metrics_b.recalls[best_index]
-    # best_precision_b = metrics_b.precisions[best_index]
-
-    # # Comparison plots
-    # plt.subplot(1, 2, 1)
-    # plt.plot(metrics_a.fprs, metrics_a.tprs, linestyle='dashed', color='darkorange', lw=1, label='AUC = %0.2f (WildGuard)' % (metrics_a.auc * 100))
-    # plt.plot(metrics_b.fprs, metrics_b.tprs, color='darkorange', lw=1, label='AUC = %0.2f (Our)' % (metrics_b.auc * 100))
-    # plt.xlim([0.0, 1.0])
-    # plt.ylim([0.0, 1.05])
-    # plt.xlabel('False Positive Rate')
-    # plt.ylabel('True Positive Rate')
-    # plt.title('Receiver Operating Characteristic (ROC)')
-    # plt.legend(loc="lower right")
-
-    # plt.subplot(1, 2, 2)
-    # plt.plot(metrics_a.thresholds, metrics_a.f1s, linestyle='dashed', color='red', lw=1, label='F1 = %0.2f (WildGuard)' % (best_f1_a * 100))
-    # plt.plot(metrics_a.thresholds, metrics_a.recalls, linestyle='dashed', color='blue', lw=1, label='Recall = %0.2f (WildGuard)' % (best_recall_a * 100))
-    # plt.plot(metrics_a.thresholds, metrics_a.precisions, linestyle='dashed', color='green', lw=1, label='Precision = %0.2f (WildGuard)' % (best_precision_a * 100))
-    # plt.plot(metrics_b.thresholds, metrics_b.f1s, color='red', lw=1, label='F1 = %0.2f (Our)' % (best_f1_b * 100))
-    # plt.plot(metrics_b.thresholds, metrics_b.recalls, color='blue', lw=1, label='Recall = %0.2f (Our)' % (best_recall_b * 100))
-    # plt.plot(metrics_b.thresholds, metrics_b.precisions, color='green', lw=1, label='Precision = %0.2f (Our)' % (best_precision_b * 100))
-    # plt.xlim([0.0, 1.05])
-    # plt.ylim([0.0, 1.05])
-    # plt.xlabel('Threshold')
-    # plt.ylabel('F1, Precision, Recall')
-    # plt.title('Performance')
-    # plt.legend(loc="lower left")
-
-    # # Add figure title
-    # plt.suptitle("JailbreakBench Dataset")
-    # plt.show()
--- src/iris/metrics/safeguard_metrics.py
+++ src/iris/metrics/safeguard_metrics.py
@@ -1,17 +1,19 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn.metrics import roc_curve, auc
+from sklearn.metrics import roc_curve, precision_recall_curve, auc
 
 
 class SafeGuardMetric:
     def __init__(self):
         self.tprs = []
         self.fprs = []
+        self.roc_thresholds = []
+        self.roc_auc = None
         self.recalls = []
         self.precisions = []
+        self.pr_thresholds = []
+        self.pr_auc = None
         self.f1s = []
-        self.thresholds = []
-        self.auc = None
 
     def update(self, gold_labels, pred_scores):
         """
@@ -22,37 +24,30 @@ class SafeGuardMetric:
         gold_labels = np.array(gold_labels)
         pred_scores = np.array(pred_scores)
         # Compute ROC curve and ROC area
-        self.fprs, self.tprs, self.thresholds = roc_curve(gold_labels, pred_scores)
-        self.auc = auc(self.fprs, self.tprs)
+        self.fprs, self.tprs, self.roc_thresholds = roc_curve(gold_labels, pred_scores)
+        self.precisions, self.recalls, self.pr_thresholds = precision_recall_curve(gold_labels, pred_scores)
+        self.roc_auc = auc(self.fprs, self.tprs)
+        self.pr_auc = auc(self.recalls, self.precisions)
         # Compute F1 score
-        self.recalls = self.tprs
-        self.precisions = np.where(self.tprs + self.fprs > 0, self.tprs / (self.tprs + self.fprs), 0)
         self.f1s = 2 * self.precisions * self.recalls / (self.precisions + self.recalls + 1e-7)
 
     def plot(self, dataset_name: str = None):
         plt.subplot(1, 2, 1)
-        plt.plot(self.fprs, self.tprs, color='darkorange', lw=1, label='AUC = %0.2f' % (self.auc * 100))
+        plt.plot(self.fprs, self.tprs, color='blue', lw=1, label='AUC = %0.2f' % (self.roc_auc * 100))
         plt.xlim([0.0, 1.0])
         plt.ylim([0.0, 1.05])
         plt.xlabel('False Positive Rate')
         plt.ylabel('True Positive Rate')
-        plt.title('Receiver Operating Characteristic')
+        plt.title('ROC Curve')
         plt.legend(loc="lower right")
 
-        best_index = np.argmax(self.f1s)
-        best_f1 = self.f1s[best_index]
-        best_recall = self.recalls[best_index]
-        best_precision = self.precisions[best_index]
-
         plt.subplot(1, 2, 2)
-        plt.plot(self.thresholds, self.f1s, color='red', lw=1, label='Best F1 = %0.2f' % (best_f1 * 100))
-        plt.plot(self.thresholds, self.recalls, color='blue', lw=1, label='Best Recall = %0.2f' % (best_recall * 100))
-        plt.plot(self.thresholds, self.precisions, color='green', lw=1, label='Best Precision = %0.2f' % (best_precision * 100))
-        plt.xlim([0.0, 1.05])
+        plt.plot(self.recalls, self.precisions, color='blue', lw=1, label='AUC = %0.2f' % (self.pr_auc * 100))
+        plt.xlim([0.0, 1.0])
         plt.ylim([0.0, 1.05])
-        plt.xlabel('Threshold')
-        plt.ylabel('F1, Precision, Recall')
-        plt.title('Performance')
+        plt.xlabel('Recall')
+        plt.ylabel('Precision')
+        plt.title('PR Curve')
         plt.legend(loc="lower right")
 
         if dataset_name:
@@ -62,53 +57,25 @@ class SafeGuardMetric:
     def plot_comparison(self, other_metrics: 'SafeGuardMetric', other_name: str, dataset_name: str = None):
         plt.figure(figsize=(12, 5))
         plt.subplot(1, 2, 1)
-        plt.plot(other_metrics.fprs, other_metrics.tprs, linestyle='dashed', color='darkorange', lw=1, label='AUC = %0.2f (%s)' % (other_metrics.auc * 100, other_name))
-        plt.plot(self.fprs, self.tprs, color='darkorange', lw=1, label='AUC = %0.2f (Our)' % (self.auc * 100))
+        plt.plot(other_metrics.fprs, other_metrics.tprs, color='red', lw=1, label='AUC = %0.2f (%s)' % (other_metrics.roc_auc * 100, other_name))
+        plt.plot(self.fprs, self.tprs, color='blue', lw=1, label='AUC = %0.2f (Our)' % (self.roc_auc * 100))
         plt.xlim([0.0, 1.0])
         plt.ylim([0.0, 1.05])
         plt.xlabel('False Positive Rate')
         plt.ylabel('True Positive Rate')
-        plt.title('Receiver Operating Characteristic')
+        plt.title('ROC Curve')
         plt.legend(loc="lower right")
 
-        best_index = np.argmax(self.f1s)
-        best_f1 = self.f1s[best_index]
-        best_recall = self.recalls[best_index]
-        best_precision = self.precisions[best_index]
-
-        best_index = np.argmax(other_metrics.f1s)
-        other_best_f1 = other_metrics.f1s[best_index]
-        other_best_recall = other_metrics.recalls[best_index]
-        other_best_precision = other_metrics.precisions[best_index]
-
         plt.subplot(1, 2, 2)
-        plt.plot(other_metrics.thresholds, other_metrics.f1s, linestyle='dashed', color='red', lw=1, label='Best F1 = %0.2f (%s)' % (other_best_f1 * 100, other_name))
-        plt.plot(other_metrics.thresholds, other_metrics.recalls, linestyle='dashed', color='blue', lw=1, label='Best Recall = %0.2f (%s)' % (other_best_recall * 100, other_name))
-        plt.plot(other_metrics.thresholds, other_metrics.precisions, linestyle='dashed', color='green', lw=1, label='Best Precision = %0.2f (%s)' % (other_best_precision * 100, other_name))
-        plt.plot(self.thresholds, self.f1s, color='red', lw=1, label='Best F1 = %0.2f (Our)' % (best_f1 * 100))
-        plt.plot(self.thresholds, self.recalls, color='blue', lw=1, label='Best Recall = %0.2f (Our)' % (best_recall * 100))
-        plt.plot(self.thresholds, self.precisions, color='green', lw=1, label='Best Precision = %0.2f (Our)' % (best_precision * 100))
-        plt.xlim([0.0, 1.05])
+        plt.plot(other_metrics.recalls, other_metrics.precisions, color='red', lw=1, label='AUC = %0.2f (%s)' % (other_metrics.pr_auc * 100, other_name))
+        plt.plot(self.recalls, self.precisions, color='blue', lw=1, label='AUC = %0.2f (Our)' % (self.pr_auc * 100))
+        plt.xlim([0.0, 1.0])
         plt.ylim([0.0, 1.05])
-        plt.xlabel('Threshold')
-        plt.ylabel('F1, Precision, Recall')
-        plt.title('Performance')
-        plt.legend(loc="lower left")
+        plt.xlabel('Recall')
+        plt.ylabel('Precision')
+        plt.title('PR Curve')
+        plt.legend(loc="lower right")
 
         if dataset_name:
             plt.suptitle(dataset_name)
         plt.show()
-
-        
-
-
-    # def plot_roc_curve(self):
-    #     plt.figure()
-    #     plt.plot(self.fprs, self.tprs, color='darkorange', lw=2, label='ROC curve (area = %0.2f)' % self.auc)
-    #     plt.xlim([0.0, 1.0])
-    #     plt.ylim([0.0, 1.05])
-    #     plt.xlabel('False Positive Rate')
-    #     plt.ylabel('True Positive Rate')
-    #     plt.title('Receiver Operating Characteristic')
-    #     plt.legend(loc="lower right")
-    #     plt.show()
