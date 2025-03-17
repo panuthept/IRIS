e6b18732225b3caf4feb4886fe21752e40520e29
Panuthep <falcon_270394@hotmail.co.th>
2025-03-17 20:25:12 +0800

    fix bug

--- inference_safeguard.sh
+++ inference_safeguard.sh
@@ -7,7 +7,7 @@
 #SBATCH --output=inference_llamaguard.out
 #SBATCH --time=1:00:00 
 #SBATCH --gres=gpu:1
-#SBATCH --nodelist=a3mega-a3meganodeset-0,4
+#SBATCH --nodelist=a3mega-a3meganodeset-${1}
 
 /home/panuthep/.conda/envs/iris/bin/python scripts/inference_safeguard.py \
 --safeguard_name LlamaGuard \
