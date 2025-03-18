55a34a182387fb645300ecaed0a18d13b5e98813
Panuthep <falcon_270394@hotmail.co.th>
2025-03-18 17:13:59 +0800

    update llamaguard back to original template

--- src/iris/model_wrappers/guard_models/llama_guard.py
+++ src/iris/model_wrappers/guard_models/llama_guard.py
@@ -246,8 +246,8 @@ class LlamaGuard(GuardLLM):
 
     @property
     def valid_outputs(self):
-        # return ["safe", "unsafe"]
-        return ["Safe", "Unsafe"]
+        return ["safe", "unsafe"]
+        # return ["Safe", "Unsafe"]
 
     def get_model_name(self) -> str:
         return self.model_name
@@ -274,7 +274,7 @@ class LlamaGuard(GuardLLM):
             [{"role": "user", "content": prompt}],
             tokenize=False,
         )
-        prompt = prompt + "\n\n"
+        # prompt = prompt + "\n\n"
         # prompt = self.sample_clf_prompt_template.format(instruction=prompt)
         response, logprobs = self.model.complete(
             prompt, 
@@ -292,8 +292,8 @@ class LlamaGuard(GuardLLM):
         tokens_logits = [logit for _, _, logit in logprobs[0]]
         tokens_probs = np.exp(tokens_logprobs) / np.sum(np.exp(tokens_logprobs))   # Convert logprobs to probabilities
 
-        # labels = ["Harmful" if token == "unsafe" else "Benign" for token, _, _ in logprobs[0] if token in self.valid_outputs]
-        labels = ["Harmful" if token == "Unsafe" else "Benign" for token, _, _ in logprobs[0] if token in self.valid_outputs]
+        labels = ["Harmful" if token == "unsafe" else "Benign" for token, _, _ in logprobs[0] if token in self.valid_outputs]
+        # labels = ["Harmful" if token == "Unsafe" else "Benign" for token, _, _ in logprobs[0] if token in self.valid_outputs]
         labels_logprobs = [logprob for token, logprob, _ in logprobs[0] if token in self.valid_outputs]
         labels_logits = [logit for token, _, logit in logprobs[0] if token in self.valid_outputs]
         labels_probs = np.exp(labels_logprobs) / np.sum(np.exp(labels_logprobs))   # Convert logprobs to probabilities
