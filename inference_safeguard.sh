02224b37d7eed56b47170a1d08991b1d0cf601c2
Panuthep <falcon_270394@hotmail.co.th>
2025-03-19 17:47:27 +0800

    inference_finetuned_llamaguard_my_vi

--- inference_safeguard.sh
+++ inference_safeguard.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-#SBATCH --job-name=inference_finetuned_llamaguard_ms_in
+#SBATCH --job-name=inference_finetuned_llamaguard_my_vi
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=10
-#SBATCH --output=inference_finetuned_llamaguard_ms_in.out
+#SBATCH --output=inference_finetuned_llamaguard_my_vi.out
 #SBATCH --time=1:00:00 
 #SBATCH --gres=gpu:1
 #SBATCH --nodelist=a3mega-a3meganodeset-2
@@ -64,58 +64,58 @@
 # --disable_logitlens \
 # --output_path ./outputs/LlamaGuard8B/SEASafeguardDataset/tl/train/all_prompts.jsonl
 
-echo "Language: ms"
-/home/panuthep/.conda/envs/iris/bin/python scripts/inference_safeguard.py \
---safeguard_name LlamaGuard \
---model_name meta-llama/Llama-Guard-3-8B \
---checkpoint_path ./data/model_checkpoints/finetuned_llamaguard/checkpoint-3500 \
---dataset_name SEASafeguardDataset \
---dataset_split train \
---language ms \
---subset general \
---max_tokens 3000 \
---max_samples 1000 \
---disable_logitlens \
---output_path ./outputs/LlamaGuard8B/SEASafeguardDataset/ms/train/all_prompts.jsonl
-
-echo "Language: in"
-/home/panuthep/.conda/envs/iris/bin/python scripts/inference_safeguard.py \
---safeguard_name LlamaGuard \
---model_name meta-llama/Llama-Guard-3-8B \
---checkpoint_path ./data/model_checkpoints/finetuned_llamaguard/checkpoint-3500 \
---dataset_name SEASafeguardDataset \
---dataset_split train \
---language in \
---subset general \
---max_tokens 3000 \
---max_samples 1000 \
---disable_logitlens \
---output_path ./outputs/LlamaGuard8B/SEASafeguardDataset/in/train/all_prompts.jsonl
-
-# echo "Language: my"
+# echo "Language: ms"
 # /home/panuthep/.conda/envs/iris/bin/python scripts/inference_safeguard.py \
 # --safeguard_name LlamaGuard \
 # --model_name meta-llama/Llama-Guard-3-8B \
 # --checkpoint_path ./data/model_checkpoints/finetuned_llamaguard/checkpoint-3500 \
 # --dataset_name SEASafeguardDataset \
 # --dataset_split train \
-# --language my \
+# --language ms \
 # --subset general \
 # --max_tokens 3000 \
 # --max_samples 1000 \
 # --disable_logitlens \
-# --output_path ./outputs/LlamaGuard8B/SEASafeguardDataset/my/train/all_prompts.jsonl
+# --output_path ./outputs/LlamaGuard8B/SEASafeguardDataset/ms/train/all_prompts.jsonl
 
-# echo "Language: vi"
+# echo "Language: in"
 # /home/panuthep/.conda/envs/iris/bin/python scripts/inference_safeguard.py \
 # --safeguard_name LlamaGuard \
 # --model_name meta-llama/Llama-Guard-3-8B \
 # --checkpoint_path ./data/model_checkpoints/finetuned_llamaguard/checkpoint-3500 \
 # --dataset_name SEASafeguardDataset \
 # --dataset_split train \
-# --language vi \
+# --language in \
 # --subset general \
 # --max_tokens 3000 \
 # --max_samples 1000 \
 # --disable_logitlens \
-# --output_path ./outputs/LlamaGuard8B/SEASafeguardDataset/vi/train/all_prompts.jsonl
+# --output_path ./outputs/LlamaGuard8B/SEASafeguardDataset/in/train/all_prompts.jsonl
+
+echo "Language: my"
+/home/panuthep/.conda/envs/iris/bin/python scripts/inference_safeguard.py \
+--safeguard_name LlamaGuard \
+--model_name meta-llama/Llama-Guard-3-8B \
+--checkpoint_path ./data/model_checkpoints/finetuned_llamaguard/checkpoint-3500 \
+--dataset_name SEASafeguardDataset \
+--dataset_split train \
+--language my \
+--subset general \
+--max_tokens 3000 \
+--max_samples 1000 \
+--disable_logitlens \
+--output_path ./outputs/LlamaGuard8B/SEASafeguardDataset/my/train/all_prompts.jsonl
+
+echo "Language: vi"
+/home/panuthep/.conda/envs/iris/bin/python scripts/inference_safeguard.py \
+--safeguard_name LlamaGuard \
+--model_name meta-llama/Llama-Guard-3-8B \
+--checkpoint_path ./data/model_checkpoints/finetuned_llamaguard/checkpoint-3500 \
+--dataset_name SEASafeguardDataset \
+--dataset_split train \
+--language vi \
+--subset general \
+--max_tokens 3000 \
+--max_samples 1000 \
+--disable_logitlens \
+--output_path ./outputs/LlamaGuard8B/SEASafeguardDataset/vi/train/all_prompts.jsonl
