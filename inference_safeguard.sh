887536dff6279a5d66458fd2602b7192c55cab00
Panuthep <falcon_270394@hotmail.co.th>
2025-03-20 13:43:53 +0800

    inference_finetuned_llamaguard_my_vi

--- inference_safeguard.sh
+++ inference_safeguard.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-#SBATCH --job-name=inference_finetuned_llamaguard_ms_in
+#SBATCH --job-name=inference_finetuned_llamaguard_my_vi
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=10
-#SBATCH --output=inference_finetuned_llamaguard_ms_in.out
+#SBATCH --output=inference_finetuned_llamaguard_my_vi.out
 #SBATCH --time=1:00:00 
 #SBATCH --gres=gpu:1
-#SBATCH --nodelist=a3mega-a3meganodeset-0
+#SBATCH --nodelist=a3mega-a3meganodeset-2
 
 # echo "Language: en"
 # /home/panuthep/.conda/envs/iris/bin/python scripts/inference_safeguard.py \
@@ -60,54 +60,54 @@
 # --disable_logitlens \
 # --output_path ./outputs/LlamaGuard8B/SEASafeguardDataset/tl/test/all_prompts.jsonl
 
-echo "Language: ms"
-/home/panuthep/.conda/envs/iris/bin/python scripts/inference_safeguard.py \
---safeguard_name LlamaGuard \
---model_name meta-llama/Llama-Guard-3-8B \
---checkpoint_path ./data/model_checkpoints/finetuned_llamaguard/checkpoint-4500 \
---dataset_name SEASafeguardDataset \
---dataset_split test \
---language ms \
---subset general \
---max_tokens 3000 \
---disable_logitlens \
---output_path ./outputs/LlamaGuard8B/SEASafeguardDataset/ms/test/all_prompts.jsonl
-
-echo "Language: in"
-/home/panuthep/.conda/envs/iris/bin/python scripts/inference_safeguard.py \
---safeguard_name LlamaGuard \
---model_name meta-llama/Llama-Guard-3-8B \
---checkpoint_path ./data/model_checkpoints/finetuned_llamaguard/checkpoint-4500 \
---dataset_name SEASafeguardDataset \
---dataset_split test \
---language in \
---subset general \
---max_tokens 3000 \
---disable_logitlens \
---output_path ./outputs/LlamaGuard8B/SEASafeguardDataset/in/test/all_prompts.jsonl
-
-# echo "Language: my"
+# echo "Language: ms"
 # /home/panuthep/.conda/envs/iris/bin/python scripts/inference_safeguard.py \
 # --safeguard_name LlamaGuard \
 # --model_name meta-llama/Llama-Guard-3-8B \
 # --checkpoint_path ./data/model_checkpoints/finetuned_llamaguard/checkpoint-4500 \
 # --dataset_name SEASafeguardDataset \
 # --dataset_split test \
-# --language my \
+# --language ms \
 # --subset general \
 # --max_tokens 3000 \
 # --disable_logitlens \
-# --output_path ./outputs/LlamaGuard8B/SEASafeguardDataset/my/test/all_prompts.jsonl
+# --output_path ./outputs/LlamaGuard8B/SEASafeguardDataset/ms/test/all_prompts.jsonl
 
-# echo "Language: vi"
+# echo "Language: in"
 # /home/panuthep/.conda/envs/iris/bin/python scripts/inference_safeguard.py \
 # --safeguard_name LlamaGuard \
 # --model_name meta-llama/Llama-Guard-3-8B \
 # --checkpoint_path ./data/model_checkpoints/finetuned_llamaguard/checkpoint-4500 \
 # --dataset_name SEASafeguardDataset \
 # --dataset_split test \
-# --language vi \
+# --language in \
 # --subset general \
 # --max_tokens 3000 \
 # --disable_logitlens \
-# --output_path ./outputs/LlamaGuard8B/SEASafeguardDataset/vi/test/all_prompts.jsonl
+# --output_path ./outputs/LlamaGuard8B/SEASafeguardDataset/in/test/all_prompts.jsonl
+
+echo "Language: my"
+/home/panuthep/.conda/envs/iris/bin/python scripts/inference_safeguard.py \
+--safeguard_name LlamaGuard \
+--model_name meta-llama/Llama-Guard-3-8B \
+--checkpoint_path ./data/model_checkpoints/finetuned_llamaguard/checkpoint-4500 \
+--dataset_name SEASafeguardDataset \
+--dataset_split test \
+--language my \
+--subset general \
+--max_tokens 3000 \
+--disable_logitlens \
+--output_path ./outputs/LlamaGuard8B/SEASafeguardDataset/my/test/all_prompts.jsonl
+
+echo "Language: vi"
+/home/panuthep/.conda/envs/iris/bin/python scripts/inference_safeguard.py \
+--safeguard_name LlamaGuard \
+--model_name meta-llama/Llama-Guard-3-8B \
+--checkpoint_path ./data/model_checkpoints/finetuned_llamaguard/checkpoint-4500 \
+--dataset_name SEASafeguardDataset \
+--dataset_split test \
+--language vi \
+--subset general \
+--max_tokens 3000 \
+--disable_logitlens \
+--output_path ./outputs/LlamaGuard8B/SEASafeguardDataset/vi/test/all_prompts.jsonl
