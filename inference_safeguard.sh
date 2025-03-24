278f827ce57a76c6703af9823394e0d1e3d1a525
Panuthep <falcon_270394@hotmail.co.th>
2025-03-24 15:44:56 +0800

    Update eval script

--- inference_safeguard.sh
+++ inference_safeguard.sh
@@ -1,46 +1,46 @@
 #!/bin/bash
 
-#SBATCH --job-name=inference_finetuned_llamaguard_culturals
+#SBATCH --job-name=inference_llamaguard_culturals
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=10
-#SBATCH --output=inference_finetuned_llamaguard_culturals.out
+#SBATCH --output=inference_llamaguard_culturals.out
 #SBATCH --time=1440:00:00 
 #SBATCH --gres=gpu:1
 #SBATCH --nodelist=a3mega-a3meganodeset-0
 
-echo "Evaluating Cultural Safety..."
-for lang in ta th tl ms in my vi
-do
-    echo "Cultural: $lang - Language: en"
-    /home/panuthep/.conda/envs/iris/bin/python scripts/inference_safeguard.py \
-    --safeguard_name LlamaGuard \
-    --model_name meta-llama/Llama-Guard-3-8B \
-    --checkpoint_path ./data/model_checkpoints/finetuned_llamaguard/checkpoint-27674 \
-    --dataset_name SEASafeguardDataset \
-    --dataset_split test \
-    --language en \
-    --cultural $lang \
-    --subset cultural_specific \
-    --mixed_tasks_sample \
-    --max_tokens 3000 \
-    --disable_logitlens \
-    --output_path ./outputs/Finetuned_LlamaGuard8B/SEASafeguardDataset/${lang}_cultural/en/test/all_prompts.jsonl
+# echo "Evaluating Cultural Safety..."
+# for lang in ta th tl ms in my vi
+# do
+#     echo "Cultural: $lang - Language: en"
+#     /home/panuthep/.conda/envs/iris/bin/python scripts/inference_safeguard.py \
+#     --safeguard_name LlamaGuard \
+#     --model_name meta-llama/Llama-Guard-3-8B \
+#     --checkpoint_path ./data/model_checkpoints/finetuned_llamaguard/checkpoint-27674 \
+#     --dataset_name SEASafeguardDataset \
+#     --dataset_split test \
+#     --language en \
+#     --cultural $lang \
+#     --subset cultural_specific \
+#     --mixed_tasks_sample \
+#     --max_tokens 3000 \
+#     --disable_logitlens \
+#     --output_path ./outputs/Finetuned_LlamaGuard8B/SEASafeguardDataset/${lang}_cultural/en/test/all_prompts.jsonl
 
-    echo "Cultural: $lang - Language: $lang"
-    /home/panuthep/.conda/envs/iris/bin/python scripts/inference_safeguard.py \
-    --safeguard_name LlamaGuard \
-    --model_name meta-llama/Llama-Guard-3-8B \
-    --checkpoint_path ./data/model_checkpoints/finetuned_llamaguard/checkpoint-27674 \
-    --dataset_name SEASafeguardDataset \
-    --dataset_split test \
-    --language $lang \
-    --cultural $lang \
-    --subset cultural_specific \
-    --mixed_tasks_sample \
-    --max_tokens 3000 \
-    --disable_logitlens \
-    --output_path ./outputs/Finetuned_LlamaGuard8B/SEASafeguardDataset/${lang}_cultural/$lang/test/all_prompts.jsonl
-done
+#     echo "Cultural: $lang - Language: $lang"
+#     /home/panuthep/.conda/envs/iris/bin/python scripts/inference_safeguard.py \
+#     --safeguard_name LlamaGuard \
+#     --model_name meta-llama/Llama-Guard-3-8B \
+#     --checkpoint_path ./data/model_checkpoints/finetuned_llamaguard/checkpoint-27674 \
+#     --dataset_name SEASafeguardDataset \
+#     --dataset_split test \
+#     --language $lang \
+#     --cultural $lang \
+#     --subset cultural_specific \
+#     --mixed_tasks_sample \
+#     --max_tokens 3000 \
+#     --disable_logitlens \
+#     --output_path ./outputs/Finetuned_LlamaGuard8B/SEASafeguardDataset/${lang}_cultural/$lang/test/all_prompts.jsonl
+# done
 
 
 echo "Evaluating General Safety..."
@@ -50,7 +50,6 @@ do
     /home/panuthep/.conda/envs/iris/bin/python scripts/inference_safeguard.py \
     --safeguard_name LlamaGuard \
     --model_name meta-llama/Llama-Guard-3-8B \
-    --checkpoint_path ./data/model_checkpoints/finetuned_llamaguard/checkpoint-27674 \
     --dataset_name SEASafeguardDataset \
     --dataset_split test \
     --language $lang \
