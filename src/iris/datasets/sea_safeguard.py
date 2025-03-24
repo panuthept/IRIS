516b641cc52479c230fbc4b0a6a1fe07b9d2d6f9
Panuthep <falcon_270394@hotmail.co.th>
2025-03-24 15:10:29 +0800

    Fix bug

--- src/iris/datasets/sea_safeguard.py
+++ src/iris/datasets/sea_safeguard.py
@@ -56,10 +56,10 @@ class SEASafeguardDataset(JailbreakDataset):
             train_dataset = load_dataset("aisingapore/SEASafeguardMix", self.language, cache_dir=self.cache_dir)
             for sample in train_dataset["train"]:
                 data["train"].append({
-                    "prompt": sample["prompt"],
-                    "response": sample["response"],
-                    "prompt_gold_label": sample["prompt_label"],
-                    "response_gold_label": sample["response_label"],
+                    "prompt": sample["prompt"] if isinstance(sample["response_label"], str) else None,
+                    "response": sample["response"] if isinstance(sample["response_label"], str) else None,
+                    "prompt_gold_label": sample["prompt_label"] if isinstance(sample["response_label"], str) else None,
+                    "response_gold_label": sample["response_label"] if isinstance(sample["response_label"], str) else None,
                 })
 
             # Load dev dataset
@@ -68,10 +68,10 @@ class SEASafeguardDataset(JailbreakDataset):
             for i in range(len(dev_dataset)):
                 sample = dev_dataset.iloc[i]
                 data["dev"].append({
-                    "prompt": sample[f"{self.language}_prompt"],
-                    "response": sample[f"{self.language}_response"],
-                    "prompt_gold_label": sample["prompt_label"],
-                    "response_gold_label": sample["response_label"],
+                    "prompt": sample[f"{self.language}_prompt"] if isinstance(sample["response_label"], str) else None,
+                    "response": sample[f"{self.language}_response"] if isinstance(sample["response_label"], str) else None,
+                    "prompt_gold_label": sample["prompt_label"] if isinstance(sample["response_label"], str) else None,
+                    "response_gold_label": sample["response_label"] if isinstance(sample["response_label"], str) else None,
                 })
 
             # Load test dataset
@@ -80,21 +80,25 @@ class SEASafeguardDataset(JailbreakDataset):
             for i in range(len(test_dataset)):
                 sample = test_dataset.iloc[i]
                 data["test"].append({
-                    "prompt": sample[f"{self.language}_prompt"],
-                    "response": sample[f"{self.language}_response"],
-                    "prompt_gold_label": sample["prompt_label"],
-                    "response_gold_label": sample["response_label"],
+                    "prompt": sample[f"{self.language}_prompt"] if isinstance(sample["response_label"], str) else None,
+                    "response": sample[f"{self.language}_response"] if isinstance(sample["response_label"], str) else None,
+                    "prompt_gold_label": sample["prompt_label"] if isinstance(sample["response_label"], str) else None,
+                    "response_gold_label": sample["response_label"] if isinstance(sample["response_label"], str) else None,
                 })
         else:
             # Load test dataset
             test_dataset = load_dataset("aisingapore/SEASafeguardMix", f"{self.cultural}_cultural_specific", cache_dir=self.cache_dir)
             # Read test dataset
             for sample in test_dataset["test"]:
+                prompt = sample["en_prompt"] if self.language == "en" else sample["local_prompt"]
+                response = sample["en_response"] if self.language == "en" else sample["local_response"]
+                prompt_gold_label = sample["prompt_label_final"] if sample["prompt_label_final"] != "Sensitive" else "Harmful" if self.sensitive_as_harmful else "Safe"
+                response_gold_label = sample["response_label_final"] if sample["response_label_final"] != "Sensitive" else "Harmful" if self.sensitive_as_harmful else "Safe"
                 data["test"].append({
-                    "prompt": sample["en_prompt"] if self.language == "en" else sample["local_prompt"],
-                    "response": sample["en_response"] if self.language == "en" else sample["local_response"],
-                    "prompt_gold_label": sample["prompt_label_final"] if sample["prompt_label_final"] != "Sensitive" else "Harmful" if self.sensitive_as_harmful else "Safe",
-                    "response_gold_label": sample["response_label_final"] if sample["response_label_final"] != "Sensitive" else "Harmful" if self.sensitive_as_harmful else "Safe",
+                    "prompt": prompt if isinstance(prompt, str) else None,
+                    "response": response if isinstance(response, str) else None,
+                    "prompt_gold_label": prompt_gold_label if isinstance(prompt, str) else None,
+                    "response_gold_label": response_gold_label if isinstance(response, str) else None,
                 })
         return data
 
