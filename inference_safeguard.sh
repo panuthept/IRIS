049d0f72031ee0c50a7bda24454093745228d325
Panuthep <falcon_270394@hotmail.co.th>
2025-03-25 15:46:05 +0800

    Update eval script

--- inference_safeguard.sh
+++ inference_safeguard.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-#SBATCH --job-name=inference_sealion_guard
+#SBATCH --job-name=inference_shieldgemma
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=10
-#SBATCH --output=inference_sealion_guard.out
+#SBATCH --output=inference_shieldgemma.out
 #SBATCH --time=1440:00:00 
 #SBATCH --gres=gpu:1
 #SBATCH --nodelist=a3mega-a3meganodeset-0
@@ -13,9 +13,8 @@ for lang in ta th tl ms in my vi
 do
     echo "Cultural: $lang - Language: en"
     /home/panuthep/.conda/envs/iris/bin/python scripts/inference_safeguard.py \
-    --safeguard_name SealionGuard \
-    --model_name aisingapore/gemma2-9b-cpt-sea-lionv3-instruct \
-    --checkpoint_path ./data/model_checkpoints/sealion_guard/checkpoint-2000 \
+    --safeguard_name ShieldGemma \
+    --model_name google/shieldgemma-9b \
     --dataset_name SEASafeguardDataset \
     --dataset_split test \
     --language en \
@@ -24,13 +23,12 @@ do
     --mixed_tasks_sample \
     --max_tokens 3000 \
     --disable_logitlens \
-    --output_path ./outputs/SealionGuard/SEASafeguardDataset/${lang}_cultural/en/test/all_prompts.jsonl
+    --output_path ./outputs/ShieldGemma/SEASafeguardDataset/${lang}_cultural/en/test/all_prompts.jsonl
 
     echo "Cultural: $lang - Language: $lang"
     /home/panuthep/.conda/envs/iris/bin/python scripts/inference_safeguard.py \
-    --safeguard_name SealionGuard \
-    --model_name aisingapore/gemma2-9b-cpt-sea-lionv3-instruct \
-    --checkpoint_path ./data/model_checkpoints/sealion_guard/checkpoint-2000 \
+    --safeguard_name ShieldGemma \
+    --model_name google/shieldgemma-9b \
     --dataset_name SEASafeguardDataset \
     --dataset_split test \
     --language $lang \
@@ -39,7 +37,7 @@ do
     --mixed_tasks_sample \
     --max_tokens 3000 \
     --disable_logitlens \
-    --output_path ./outputs/SealionGuard/SEASafeguardDataset/${lang}_cultural/$lang/test/all_prompts.jsonl
+    --output_path ./outputs/ShieldGemma/SEASafeguardDataset/${lang}_cultural/$lang/test/all_prompts.jsonl
 done
 
 
@@ -48,9 +46,8 @@ for lang in en ta th tl ms in my vi
 do
     echo "Language: $lang"
     /home/panuthep/.conda/envs/iris/bin/python scripts/inference_safeguard.py \
-    --safeguard_name SealionGuard \
-    --model_name aisingapore/gemma2-9b-cpt-sea-lionv3-instruct \
-    --checkpoint_path ./data/model_checkpoints/sealion_guard/checkpoint-2000 \
+    --safeguard_name ShieldGemma \
+    --model_name google/shieldgemma-9b \
     --dataset_name SEASafeguardDataset \
     --dataset_split test \
     --language $lang \
@@ -58,5 +55,5 @@ do
     --mixed_tasks_sample \
     --max_tokens 3000 \
     --disable_logitlens \
-    --output_path ./outputs/SealionGuard/SEASafeguardDataset/general/$lang/test/all_prompts.jsonl
+    --output_path ./outputs/ShieldGemma/SEASafeguardDataset/general/$lang/test/all_prompts.jsonl
 done
