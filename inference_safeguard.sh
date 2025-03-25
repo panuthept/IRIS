8cc82e70f8750699fb5a870e7929bc536b4e9381
Panuthep <falcon_270394@hotmail.co.th>
2025-03-25 16:26:22 +0800

    Update eval script

--- inference_safeguard.sh
+++ inference_safeguard.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-#SBATCH --job-name=inference_shieldgemma
+#SBATCH --job-name=inference_wildguard
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=10
-#SBATCH --output=inference_shieldgemma.out
+#SBATCH --output=inference_wildguard.out
 #SBATCH --time=1440:00:00 
 #SBATCH --gres=gpu:1
 #SBATCH --nodelist=a3mega-a3meganodeset-0
@@ -13,8 +13,8 @@ for lang in ta th tl ms in my vi
 do
     echo "Cultural: $lang - Language: en"
     /home/panuthep/.conda/envs/iris/bin/python scripts/inference_safeguard.py \
-    --safeguard_name ShieldGemma \
-    --model_name google/shieldgemma-9b \
+    --safeguard_name WildGuard \
+    --model_name allenai/wildguard \
     --dataset_name SEASafeguardDataset \
     --dataset_split test \
     --language en \
@@ -23,12 +23,12 @@ do
     --mixed_tasks_sample \
     --max_tokens 3000 \
     --disable_logitlens \
-    --output_path ./outputs/ShieldGemma/SEASafeguardDataset/${lang}_cultural/en/test/all_prompts.jsonl
+    --output_path ./outputs/WildGuard/SEASafeguardDataset/${lang}_cultural/en/test/all_prompts.jsonl
 
     echo "Cultural: $lang - Language: $lang"
     /home/panuthep/.conda/envs/iris/bin/python scripts/inference_safeguard.py \
-    --safeguard_name ShieldGemma \
-    --model_name google/shieldgemma-9b \
+    --safeguard_name WildGuard \
+    --model_name allenai/wildguard \
     --dataset_name SEASafeguardDataset \
     --dataset_split test \
     --language $lang \
@@ -37,7 +37,7 @@ do
     --mixed_tasks_sample \
     --max_tokens 3000 \
     --disable_logitlens \
-    --output_path ./outputs/ShieldGemma/SEASafeguardDataset/${lang}_cultural/$lang/test/all_prompts.jsonl
+    --output_path ./outputs/WildGuard/SEASafeguardDataset/${lang}_cultural/$lang/test/all_prompts.jsonl
 done
 
 
@@ -46,8 +46,8 @@ for lang in en ta th tl ms in my vi
 do
     echo "Language: $lang"
     /home/panuthep/.conda/envs/iris/bin/python scripts/inference_safeguard.py \
-    --safeguard_name ShieldGemma \
-    --model_name google/shieldgemma-9b \
+    --safeguard_name WildGuard \
+    --model_name allenai/wildguard \
     --dataset_name SEASafeguardDataset \
     --dataset_split test \
     --language $lang \
@@ -55,5 +55,5 @@ do
     --mixed_tasks_sample \
     --max_tokens 3000 \
     --disable_logitlens \
-    --output_path ./outputs/ShieldGemma/SEASafeguardDataset/general/$lang/test/all_prompts.jsonl
+    --output_path ./outputs/WildGuard/SEASafeguardDataset/general/$lang/test/all_prompts.jsonl
 done
