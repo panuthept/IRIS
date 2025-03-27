7e9d2c324e1a2566cfbbbb11ad64a2ac41745fba
Panuthep <falcon_270394@hotmail.co.th>
2025-03-27 14:25:56 +0800

    Update eval script

--- inference_safeguard.sh
+++ inference_safeguard.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-#SBATCH --job-name=inference_nemoguard
+#SBATCH --job-name=inference_llamaguard
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=10
-#SBATCH --output=inference_nemoguard.out
+#SBATCH --output=inference_llamaguard.out
 #SBATCH --time=1440:00:00 
-#SBATCH --gres=gpu:1
+#SBATCH --gres=gpu:3
 #SBATCH --nodelist=a3mega-a3meganodeset-0
 
 echo "Evaluating Cultural Safety..."
@@ -13,8 +13,8 @@ for lang in ta th tl ms in my vi
 do
     echo "Cultural: $lang - Language: en"
     /home/panuthep/.conda/envs/iris/bin/python scripts/inference_safeguard.py \
-    --safeguard_name NemoGuard \
-    --model_name meta-llama/Llama-3.1-8B-Instruct \
+    --safeguard_name LlamaGuard \
+    --model_name meta-llama/Llama-Guard-3-8B \
     --checkpoint_path ./data/models/llama-3.1-nemoguard-8b-content-safety-lora-adapter \
     --dataset_name SEASafeguardDataset \
     --dataset_split test \
@@ -22,14 +22,14 @@ do
     --cultural $lang \
     --subset cultural_specific \
     --mixed_tasks_sample \
-    --max_tokens 3000 \
+    --max_tokens 8000 \
     --disable_logitlens \
-    --output_path ./outputs/NemoGuard/SEASafeguardDataset/${lang}_cultural/en/test/all_prompts.jsonl
+    --output_path ./outputs/LlamaGuard/SEASafeguardDataset/${lang}_cultural/en/test/all_prompts.jsonl
 
     echo "Cultural: $lang - Language: $lang"
     /home/panuthep/.conda/envs/iris/bin/python scripts/inference_safeguard.py \
-    --safeguard_name NemoGuard \
-    --model_name meta-llama/Llama-3.1-8B-Instruct \
+    --safeguard_name LlamaGuard \
+    --model_name meta-llama/Llama-Guard-3-8B \
     --checkpoint_path ./data/models/llama-3.1-nemoguard-8b-content-safety-lora-adapter \
     --dataset_name SEASafeguardDataset \
     --dataset_split test \
@@ -37,9 +37,9 @@ do
     --cultural $lang \
     --subset cultural_specific \
     --mixed_tasks_sample \
-    --max_tokens 3000 \
+    --max_tokens 8000 \
     --disable_logitlens \
-    --output_path ./outputs/NemoGuard/SEASafeguardDataset/${lang}_cultural/$lang/test/all_prompts.jsonl
+    --output_path ./outputs/LlamaGuard/SEASafeguardDataset/${lang}_cultural/$lang/test/all_prompts.jsonl
 done
 
 
@@ -48,15 +48,15 @@ for lang in en ta th tl ms in my vi
 do
     echo "Language: $lang"
     /home/panuthep/.conda/envs/iris/bin/python scripts/inference_safeguard.py \
-    --safeguard_name NemoGuard \
-    --model_name meta-llama/Llama-3.1-8B-Instruct \
+    --safeguard_name LlamaGuard \
+    --model_name meta-llama/Llama-Guard-3-8B \
     --checkpoint_path ./data/models/llama-3.1-nemoguard-8b-content-safety-lora-adapter \
     --dataset_name SEASafeguardDataset \
     --dataset_split test \
     --language $lang \
     --subset general \
     --mixed_tasks_sample \
-    --max_tokens 3000 \
+    --max_tokens 8000 \
     --disable_logitlens \
-    --output_path ./outputs/NemoGuard/SEASafeguardDataset/general/$lang/test/all_prompts.jsonl
+    --output_path ./outputs/LlamaGuard/SEASafeguardDataset/general/$lang/test/all_prompts.jsonl
 done
