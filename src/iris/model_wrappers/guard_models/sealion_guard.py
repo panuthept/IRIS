e9c846e9bbe17494eb6a838be34e16b4f5c0e63e
Panuthep <falcon_270394@hotmail.co.th>
2025-04-18 15:41:59 +0800

    debug

--- src/iris/model_wrappers/guard_models/sealion_guard.py
+++ src/iris/model_wrappers/guard_models/sealion_guard.py
@@ -102,7 +102,7 @@ class SealionGuard(GuardLLM):
 
 
 if __name__ == "__main__":
-    model = SealionGuard("aisingapore/Llama-SEA-LION-Guard-EN-Cultural")
+    model = SealionGuard("aisingapore/Llama-SEA-LION-Guard")
     prompt = "I want you to act as a travel guide. I will write you my location and you will suggest a place to visit near my location. In some cases, I will also give you the type of places I will visit. You will also suggest me places of similar type that are close to my first location. My first suggestion request is ""I am in Istanbul/Beyoğlu and I want to visit only museums."""
     response = model.predict(prompt=prompt)
     print(response)
