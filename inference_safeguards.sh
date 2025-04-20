38b1f1f3c6d69cd6176dc9dd05c8cbc263e2fafa
Panuthep <falcon_270394@hotmail.co.th>
2025-04-20 15:49:13 +0800

    skip en

--- inference_safeguards.sh
+++ inference_safeguards.sh
@@ -3,11 +3,15 @@
 echo "Evaluating ShieldGemma..."
 sh inference_safeguard.sh ShieldGemma google/shieldgemma-9b ShieldGemma > inference_safeguard_shieldgemma.out
 
+echo "Evaluating PolyGuard..."
+sh inference_safeguard.sh PolyGuard ToxicityPrompts/PolyGuard-Qwen-Smol PolyGuard > inference_safeguard_polyguard.out
+
 echo "Evaluating LlamaGuard..."
 sh inference_safeguard.sh LlamaGuard meta-llama/Llama-Guard-3-8B LlamaGuard > inference_safeguard_llamaguard.out
 
 echo "Evaluating WildGuard..."
 sh inference_safeguard.sh WildGuard allenai/wildguard WildGuard > inference_safeguard_wildguard.out
+sh multigpu_inference_safeguard.sh WildGuard allenai/wildguard WildGuard > inference_safeguard_wildguard.out
 
 echo "Evaluating Llama-SealionGuard..."
 sh inference_safeguard.sh SealionGuard aisingapore/Llama-SEA-LION-Guard Llama-SealionGuard > inference_safeguard_llama_sealionguard.out
@@ -20,3 +24,7 @@ sh inference_safeguard.sh SealionGuard aisingapore/Llama-SEA-LION-Guard-EN-Cultu
 
 echo "Evaluating Llama-SEA-LION-Guard-THEN-Cultural..."
 sh inference_safeguard.sh SealionGuard aisingapore/Llama-SEA-LION-Guard-THEN-Cultural Llama-SEA-LION-Guard-THEN-Cultural > inference_safeguard_llama_sealionguard_then_cultural.out
+
+echo "Evaluating Llama-SEA-LION-Guard-TH-Cultural..."
+sh inference_safeguard.sh SealionGuard aisingapore/Llama-SEA-LION-Guard-TH-Cultural Llama-SEA-LION-Guard-TH-Cultural > inference_safeguard_llama_sealionguard_th_cultural.out
+sh multigpu_inference_safeguard.sh SealionGuard aisingapore/Llama-SEA-LION-Guard-TH-Cultural Llama-SEA-LION-Guard-TH-Cultural > inference_safeguard_llama_sealionguard_th_cultural.out
