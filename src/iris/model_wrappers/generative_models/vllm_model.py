bd9235a392bdeefaed3b2121d098f4d8ef4db58a
Panuthep <falcon_270394@hotmail.co.th>
2025-04-25 14:39:03 +0800

    Clean code in vLLM

--- src/iris/model_wrappers/generative_models/vllm_model.py
+++ src/iris/model_wrappers/generative_models/vllm_model.py
@@ -38,28 +38,6 @@ class vLLM(LLM):
         logprobs = [[(v.decoded_token, v.logprob, None) for k, v in logprob.items()] for logprob in outputs[0].outputs[0].logprobs]
         return answer, logprobs
 
-        # completed_ids, logprobs, logits = self._generate(
-        #     input_ids=encoded_texts["input_ids"],
-        #     attention_mask=encoded_texts["attention_mask"],
-        #     max_new_tokens=self.max_new_tokens,
-        #     temperature=self.temperature,
-        #     return_logprobs=self.logprobs,
-        #     top_k=self.top_logprobs,
-        #     **kwargs,
-        # )
-        # pred_ids = completed_ids[0]
-        # logprobs = logprobs[0]
-        # logits = logits[0]
-        # sorted_logprobs, sorted_indices = torch.sort(logprobs, descending=True)
-        # sorted_logits = logits[torch.arange(logits.size(0)).unsqueeze(1), sorted_indices]
-        # sorted_logprobs = sorted_logprobs[:, :self.top_logprobs]
-        # sorted_logits = sorted_logits[:, :self.top_logprobs]
-        # sorted_indices = sorted_indices[:, :self.top_logprobs]
-        # # Decode the response
-        # answer = self.tokenizer.decode(pred_ids, skip_special_tokens=True)
-        # logprobs = [[(self.id_to_token(token_id), logprob.item(), logit.item()) for token_id, logprob, logit in zip(top_indices, top_logprobs, top_logits)] for top_indices, top_logprobs, top_logits in zip(sorted_indices, sorted_logprobs, sorted_logits)]
-        # return answer, logprobs
-
 
 if __name__ == "__main__":
     # Example usage
