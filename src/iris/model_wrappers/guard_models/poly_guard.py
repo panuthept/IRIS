896f0c5895aab4a1151fce86cfa40cdd45806657
Panuthep <falcon_270394@hotmail.co.th>
2025-04-27 20:00:36 +0800

    debug

--- src/iris/model_wrappers/guard_models/poly_guard.py
+++ src/iris/model_wrappers/guard_models/poly_guard.py
@@ -156,16 +156,16 @@ class PolyGuard(GuardLLM):
         metadata = {}
         # Prompt classification
         prompt_clf: Dict[str, List[Tuple[str, float, float]]] = self._prompt_classify(prompt, **kwargs)
-        prompt_labels = prompt_clf["pred_labels"][0]
-        metadata["prompt_tokens"] = prompt_clf["pred_tokens"][0]
+        prompt_labels = prompt_clf["pred_labels"][0] if len(prompt_clf["pred_labels"]) > 0 else []
+        metadata["prompt_tokens"] = prompt_clf["pred_tokens"][0] if len(prompt_clf["pred_tokens"]) > 0 else []
         metadata["prompt_instruction"] = prompt_clf["instruction"]
         metadata["prompt_response"] = prompt_clf["response"]
         # Response classification
         response_labels = None
         if response is not None:
             response_clf: Dict[str, List[Tuple[str, float, float]]] = self._response_classify(prompt, response, **kwargs)
-            response_labels = response_clf["pred_labels"][2]
-            metadata["response_tokens"] = response_clf["pred_tokens"][2]
+            response_labels = response_clf["pred_labels"][2] if len(response_clf["pred_labels"]) > 2 else []
+            metadata["response_tokens"] = response_clf["pred_tokens"][2] if len(response_clf["pred_tokens"]) > 2 else []
             metadata["response_instruction"] = response_clf["instruction"]
             metadata["response_response"] = response_clf["response"]
         # Output formatting
