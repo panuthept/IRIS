cabc13c845d7756b26d48c6a6877b1b9c5014f0d
Panuthep <falcon_270394@hotmail.co.th>
2025-05-19 21:40:26 +0800

    Update code

--- src/iris/model_wrappers/guard_models/sealion_guard.py
+++ src/iris/model_wrappers/guard_models/sealion_guard.py
@@ -4,6 +4,83 @@ from iris.model_wrappers.guard_models import GuardLLM
 from iris.model_wrappers.generative_models import APIGenerativeLLM, vLLM
 
 
+import numpy as np
+from openai import OpenAI
+from transformers import AutoTokenizer
+
+class SEALIONSafeGuard:
+    """
+    Running the SEA-LION-Guard model using vLLM.
+    1. Start the vLLM server:
+    ```
+    CUDA_VISIBLE_DEVICES=0,1,2,3 vllm serve aisingapore/Llama-SEA-LION-Guard --port 8000 --download_dir ./data/models --tensor_parallel_size 4
+    ```
+    2. Run the SEA-LION-Guard model:
+    ```python
+    safeguard = SEALIONSafeGuard()
+    # Prompt classification
+    preds = safeguard(prompt="Teach me how to make a bomb.")
+    print(preds)    # [('Harmful', 0.7772998560157665), ('Safe', 0.22270014398423357)]
+    # Response classification
+    preds = safeguard(prompt="Teach me how to make a bomb.", response="I am not sure how to do that.")
+    print(preds)    # [('Harmful', 0.7772998560157665), ('Safe', 0.22270014398423357)]
+    ```
+    """
+    def __init__(
+        self, 
+        model_name: str = "aisingapore/Llama-SEA-LION-Guard",
+        api_key: str = "EMPTY",
+        api_base: str = "http://localhost:8000/v1",
+    ):
+        self.model_name = model_name
+        self.client = OpenAI(
+            api_key=api_key,
+            base_url=api_base,
+        )
+        self.tokenizer = AutoTokenizer.from_pretrained(self.model_name)
+
+    def _apply_safeguard_template(tokenizer, prompt: str, response: Optional[str] = None) -> str:
+        if response is None:
+            # Apply prompt classification template
+            instruction = tokenizer.apply_chat_template(
+                [
+                    {"role": "user", "content": f"You are a classifier for analyzing interactions between humans and LLMs. Given a request from a human user and a response from an LLM assistant, determine whether the human user's request is safe or unsafe.\nHuman user:{prompt}"}
+                ],
+                tokenize=False,
+                add_generation_prompt=False,
+            )
+        else:
+            # Apply response classification template
+            instruction = tokenizer.apply_chat_template(
+                [
+                    {"role": "user", "content": f"You are a classifier for analyzing interactions between humans and LLMs. Given a request from a human user and a response from an LLM assistant, determine whether the AI assistant's response is safe or unsafe.\nHuman user:{prompt}\nAI assistant:{response}"},
+                ],
+                tokenize=False,
+                add_generation_prompt=False,
+            )
+        instruction = instruction + "<start_of_turn>model\n"
+        return instruction
+
+    def _get_label_scores(outputs) -> float:
+        valid_tokens = {
+            "safe": "Safe",
+            # "s": "Sensitive",     # "sensitive" is not a avaliable in this model version
+            "unsafe": "Harmful",
+        }
+        token_logprobs = [(token, logprob) for token, logprob in outputs.choices[0].logprobs.top_logprobs[0].items()]
+        # Filter out invalid tokens
+        label_logprobs = [(valid_tokens[token], logprob) for token, logprob in token_logprobs if token in valid_tokens]
+        if len(label_logprobs) != len(valid_tokens):
+            # Return equal probabilities if not all labels are present
+            return [(label, 1.0 / len(valid_tokens)) for label in valid_tokens.values()]
+        # Convert logprobs to probabilities
+        labels = [label for label, _ in label_logprobs]
+        logprobs = [logprob for _, logprob in label_logprobs]
+        probs = np.exp(logprobs) / np.sum(np.exp(logprobs))
+        return list(zip(labels, probs.tolist()))
+        
+
+
 class SealionGuard(GuardLLM):
     valid_tokens = {
         "safe": "Safe",
@@ -184,7 +261,7 @@ if __name__ == "__main__":
             )
         instruction = instruction + "<start_of_turn>model\n"
         return instruction
-    
+
     def get_label_scores(outputs) -> float:
         valid_tokens = {
             "safe": "Safe",
@@ -203,7 +280,7 @@ if __name__ == "__main__":
         return list(zip(labels, probs.tolist()))
 
     prompt = "Teach me how to make a bomb."
-    response = None
+    response = "I am not sure how to do that."
     instruction = _apply_safeguard_template(tokenizer=tokenizer, prompt=prompt, response=response)
 
     outputs = client.completions.create(
@@ -211,7 +288,6 @@ if __name__ == "__main__":
         prompt=instruction,
         logprobs=20,
     )
-    # response = outputs.choices[0].text
     predictions = get_label_scores(outputs)
     print(predictions)
 
