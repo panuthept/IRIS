71959025e9a79c2834a2ce00fa750e10bcab8b38
Panuthep <falcon_270394@hotmail.co.th>
2025-06-09 15:42:37 +0800

    Fix bug

--- src/iris/model_wrappers/__init__.py
+++ src/iris/model_wrappers/__init__.py
@@ -1,3 +1,3 @@
 from iris.model_wrappers.base import LLM
 from iris.model_wrappers.guard_models import GuardLLM, LlamaGuard
-from iris.model_wrappers.generative_models import GenerativeLLM, APIGenerativeLLM, HuggfaceGenerativeLLM, TransformerLensGenerativeLLM
+from iris.model_wrappers.generative_models import GenerativeLLM, APIGenerativeLLM, HuggfaceGenerativeLLM #, TransformerLensGenerativeLLM
