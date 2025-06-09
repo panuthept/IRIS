e1980815ab8dc125abc7ddb658b628438defc809
Panuthep <falcon_270394@hotmail.co.th>
2025-06-09 15:38:59 +0800

    fix bug

--- src/iris/model_wrappers/generative_models/__init__.py
+++ src/iris/model_wrappers/generative_models/__init__.py
@@ -2,4 +2,4 @@ from iris.model_wrappers.generative_models.vllm_model import vLLM
 from iris.model_wrappers.generative_models.base import GenerativeLLM
 from iris.model_wrappers.generative_models.api_model import APIGenerativeLLM
 from iris.model_wrappers.generative_models.huggingface_model import HuggfaceGenerativeLLM
-from iris.model_wrappers.generative_models.transformer_lens_model import TransformerLensGenerativeLLM
+# from iris.model_wrappers.generative_models.transformer_lens_model import TransformerLensGenerativeLLM
