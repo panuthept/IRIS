955c3dede4c3d667a0da8a1dbc874df76753660a
Panuthep <falcon_270394@hotmail.co.th>
2025-06-18 19:30:54 +0800

    debug

--- src/iris/model_wrappers/generative_models/huggingface_model.py
+++ src/iris/model_wrappers/generative_models/huggingface_model.py
@@ -556,11 +556,8 @@ class HuggfaceGenerativeLLM:
             top_token_ids = torch.argsort(logprobs, dim=-1, descending=True)[:, :self.top_logprobs]
             top_logprobs = logprobs.gather(dim=-1, index=top_token_ids)
             top_tokens = self.tokenizer.convert_ids_to_tokens(top_token_ids[0].tolist())
-            print(logprobs)  
-            print(top_token_ids) 
-            print(top_tokens)  
-            print(top_logprobs)
-            print("-" * 100)   
+            lst_logprobs.append([(token, logprob, None) for token, logprob in zip(top_tokens, top_logprobs[0].tolist())])
+            print(lst_logprobs[-1])  
 
         answer = self.tokenizer.decode(result.sequences[0][len(model_input['input_ids'][0]):], skip_special_tokens=True)
         print(answer)
