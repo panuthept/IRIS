37e7dc150c76156bdbb6bf44ecbef185f3d6a1e1
Panuthep <falcon_270394@hotmail.co.th>
2025-06-21 00:18:26 +0800

    Add GPT4o

--- src/iris/model_wrappers/guard_models/__init__.py
+++ src/iris/model_wrappers/guard_models/__init__.py
@@ -1,6 +1,6 @@
 from typing import Optional, Union
 from iris.model_wrappers.guard_models.base import GuardLLM, PromptGuard
-from iris.model_wrappers.guard_models.llm_guard import LLMGuard
+from iris.model_wrappers.guard_models.llm_guard import LLMGuard, GPT4o
 from iris.model_wrappers.guard_models.poly_guard import PolyGuard
 # from iris.model_wrappers.guard_models.nemo_guard import NemoGuard
 # from iris.model_wrappers.guard_models.wild_guard import WildGuard
@@ -13,6 +13,7 @@ from iris.model_wrappers.guard_models.llama_guard import LlamaGuard, LlamaGuard4
 AVAILABLE_GUARDS = {
     # "NemoGuard": NemoGuard,
     # "WildGuard": WildGuard,
+    "GPT4o": GPT4o,
     "LLMGuard": LLMGuard,
     "LlamaGuard": LlamaGuard,
     "LlamaGuard4": LlamaGuard4,
