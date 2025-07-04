66e8f6c4f0902e17e795d9fdfdfdc714bc7c7a59
Panuthep <falcon_270394@hotmail.co.th>
2025-07-04 17:25:35 +0800

    fix bug

--- api_eval_llm_refusal.sh
+++ api_eval_llm_refusal.sh
@@ -7,25 +7,25 @@ api_base=$4
 
 # Start VLLM server before running this script
 
-echo "Evaluating Cultural Safety (Handwritten)..."
-for lang in ta th tl ms in my vi
-do
-    echo "Cultural: $lang - Language: en"
-    python scripts/eval_llm_refusal.py \
-    --load_path ./outputs/${load_path}/SEASafeguardDataset/${lang}_cultural_handwritten/en/test/all_prompts.jsonl \
-    --save_path ./outputs/${load_path}/SEASafeguardDataset/${lang}_cultural_handwritten/en/test/eval_refusal.jsonl \
-    --model_name $judge_name \
-    --api_key $api_key \
-    --api_base $api_base 
+# echo "Evaluating Cultural Safety (Handwritten)..."
+# for lang in ta th tl ms in my vi
+# do
+#     echo "Cultural: $lang - Language: en"
+#     python scripts/eval_llm_refusal.py \
+#     --load_path ./outputs/${load_path}/SEASafeguardDataset/${lang}_cultural_handwritten/en/test/all_prompts.jsonl \
+#     --save_path ./outputs/${load_path}/SEASafeguardDataset/${lang}_cultural_handwritten/en/test/eval_refusal.jsonl \
+#     --model_name $judge_name \
+#     --api_key $api_key \
+#     --api_base $api_base 
 
-    echo "Cultural: $lang - Language: $lang"
-    python scripts/eval_llm_refusal.py \
-    --load_path ./outputs/${load_path}/SEASafeguardDataset/${lang}_cultural_handwritten/$lang/test/all_prompts.jsonl \
-    --save_path ./outputs/${load_path}/SEASafeguardDataset/${lang}_cultural_handwritten/$lang/test/eval_refusal.jsonl \
-    --model_name $judge_name \
-    --api_key $api_key \
-    --api_base $api_base
-done
+#     echo "Cultural: $lang - Language: $lang"
+#     python scripts/eval_llm_refusal.py \
+#     --load_path ./outputs/${load_path}/SEASafeguardDataset/${lang}_cultural_handwritten/$lang/test/all_prompts.jsonl \
+#     --save_path ./outputs/${load_path}/SEASafeguardDataset/${lang}_cultural_handwritten/$lang/test/eval_refusal.jsonl \
+#     --model_name $judge_name \
+#     --api_key $api_key \
+#     --api_base $api_base
+# done
 
 
 echo "Evaluating Cultural Safety..."
@@ -37,7 +37,7 @@ do
     --save_path ./outputs/${load_path}/SEASafeguardDataset/${lang}_cultural/en/test/eval_refusal.jsonl \
     --model_name $judge_name \
     --api_key $api_key \
-    --api_base $api_base \
+    --api_base $api_base
 
     echo "Cultural: $lang - Language: $lang"
     python scripts/eval_llm_refusal.py \
@@ -45,7 +45,8 @@ do
     --save_path ./outputs/${load_path}/SEASafeguardDataset/${lang}_cultural/$lang/test/eval_refusal.jsonl \
     --model_name $judge_name \
     --api_key $api_key \
-    --api_base $api_base \
+    --api_base $api_base
+done
 
 
 echo "Evaluating General Safety..."
@@ -57,5 +58,5 @@ do
     --save_path ./outputs/${load_path}/SEASafeguardDataset/general/$lang/test/eval_refusal.jsonl \
     --model_name $judge_name \
     --api_key $api_key \
-    --api_base $api_base \
+    --api_base $api_base
 done
