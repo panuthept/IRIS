6da23de2364150d3088c256688181bc0c69c8577
Panuthep <falcon_270394@hotmail.co.th>
2025-07-14 12:24:03 +0800

    Add response refinement script

---
+++ scripts/refine_llm_response_with_safeguard.py
@@ -0,0 +1,225 @@
+import os
+import json
+import torch
+import hashlib
+import argparse
+from tqdm import tqdm
+from google import genai
+from openai import OpenAI
+from typing import List, Dict
+from transformers import AutoModelForCausalLM, AutoTokenizer
+
+
+def hash_prompt(prompt: str) -> str:
+    """Generate a sha256 hash for the prompt."""
+    return hashlib.sha256(prompt.encode('utf-8')).hexdigest()
+
+def get_safeguard_label(prompt_labels: List[str]) -> str:
+    """Get the safeguard label from the prompt labels."""
+    pred_label = None
+    pred_score = 0.0
+    for label, score, _ in prompt_labels:
+        if score > pred_score:
+            pred_label = label
+            pred_score = score
+    return pred_label
+
+class APIModel:
+    def __init__(
+            self, 
+            model_name: str, 
+            api_key: str = None, 
+            api_base: str = None,
+    ):
+        self.model_name = model_name
+        self.client = OpenAI(api_key=api_key, base_url=api_base)
+
+    def complete(self, prompt: str = None, messages: List[Dict] = None):
+        if messages is not None:
+            outputs = self.client.chat.completions.create(
+                model=self.model_name,
+                messages=messages,
+                max_tokens=2048,
+            )
+            response = outputs.choices[0].message.content
+        else:
+            outputs = self.client.completions.create(
+                model=self.model_name,
+                prompt=prompt,
+                max_tokens=2048,
+            )
+            response = outputs.choices[0].text
+        return response
+    
+class GeminiAPIModel:
+    def __init__(self, model_name: str, project: str, location: str):
+        self.model_name = model_name
+        self.client = genai.Client(
+            vertexai=True, project=project, location=location,
+        )
+
+    def complete(self, prompt: str = None, messages: List[Dict] = None):
+        assert messages is None, "Gemini API does not support messages format. Please use prompt format instead."
+        response = self.client.models.generate_content(
+            model=self.model_name,
+            contents=[prompt]
+        )
+        return response.text
+    
+class HFModel:
+    def __init__(self, model_name: str, tokenizer_name: str = None):
+        self.model_name = model_name
+        self.tokenizer = self.load_tokenizer(model_name) if tokenizer_name is None else self.load_tokenizer(tokenizer_name)
+        self.model = self.load_model(model_name)
+
+    def load_tokenizer(self, model_name_or_path: str):
+        try:
+            tokenizer = AutoTokenizer.from_pretrained(
+                model_name_or_path,
+                cache_dir="./data/models",
+                local_files_only=True,
+            )
+        except:
+            tokenizer = AutoTokenizer.from_pretrained(
+                model_name_or_path,
+                cache_dir="./data/models",
+                local_files_only=False,
+            )
+        return tokenizer
+    
+    def load_model(self, model_name_or_path: str):
+        try:
+            model = AutoModelForCausalLM.from_pretrained(
+                model_name_or_path,
+                cache_dir="./data/models",
+                tp_plan="auto",
+                local_files_only=True,
+            )
+        except:
+            try:
+                model = AutoModelForCausalLM.from_pretrained(
+                    model_name_or_path,
+                    cache_dir="./data/models",
+                    tp_plan="auto",
+                    local_files_only=False,
+                )
+            except:
+                model = AutoModelForCausalLM.from_pretrained(
+                    model_name_or_path,
+                    cache_dir="./data/models",
+                    device_map="auto",
+                    local_files_only=False,
+                )
+        model.eval()  # Set the model to evaluation mode
+        return model
+
+    def complete(self, prompt: str = None, messages: List[Dict] = None):
+        if messages is not None:
+            prompt = self.tokenizer.apply_chat_template(
+                messages, 
+                tokenize=False, 
+                add_generation_prompt=True, 
+                return_dict=False
+            )
+        inputs = self.tokenizer(
+            prompt,  
+            return_tensors="pt"
+        )
+        # Move input tensors to the same device as the model
+        for key in inputs.keys():
+            if isinstance(inputs[key], torch.Tensor):
+                inputs[key] = inputs[key].to(self.model.device)
+        # Generate the response (return logprobs)
+        with torch.no_grad():
+            outputs = self.model.generate(
+                **inputs, 
+                max_new_tokens=self.max_new_tokens,
+                return_dict_in_generate=True,
+                output_logits=True,
+            )
+            response = self.tokenizer.decode(outputs.sequences[0][len(inputs['input_ids'][0]):], skip_special_tokens=True)
+        return response
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--model_name", type=str, default="google/gemma-3-27b-it")
+    parser.add_argument("--tokenizer_name", type=str, default="google/gemma-3-27b-it")
+    parser.add_argument("--api_key", type=str, default=None)
+    parser.add_argument("--api_base", type=str, default=None)
+    parser.add_argument("--project", type=str, default=None)
+    parser.add_argument("--location", type=str, default=None)
+    parser.add_argument("--input_path", type=str, default="./outputs/gemma-3-27b-it-LlamaGuard/SEASafeguardDataset/general/en/test/eval_safe_response.jsonl")
+    parser.add_argument("--output_path", type=str, default="./outputs/gemma-3-27b-it-LlamaGuard-RefinedResponse/SEASafeguardDataset/general/en/test/refined_responses.jsonl")
+    args = parser.parse_args()
+
+    # Initial model
+    if args.api_key is not None and args.api_base is not None:
+        # Use OpenAI API
+        llm = APIModel(
+            model_name=args.model_name,
+            api_key=args.api_key,
+            api_base=args.api_base,
+        )
+    elif args.project is not None and args.location is not None:
+        # Use Gemini API
+            llm = GeminiAPIModel(
+                model_name=args.model_name,
+                project=args.project,
+                location=args.location,
+            )
+    else:
+        # Use Hugging Face API
+        llm = HFModel(
+            model_name=args.model_name,
+            tokenizer_name=args.tokenizer_name,
+        )
+
+    # Create save directory
+    os.makedirs(os.path.dirname(args.output_path), exist_ok=True)
+
+    # # Load existing results if available
+    count = 0
+    if os.path.exists(args.output_path):
+        with open(args.output_path, "r") as f:
+            for line in f:
+                if line.strip() != "":
+                    count += 1
+
+    with open(args.output_path, "a") as out_file:
+        with open(args.input_path, "r") as f:
+            for sample_id, line in tqdm(enumerate(f)):
+                if line.strip() == "":
+                    continue
+                if sample_id < count:
+                    continue
+                example = json.loads(line.strip())
+                prompt = example["prompt"]
+                prompt_label = get_safeguard_label(example["prompt_labels"])
+                # Get refined responses
+                responses = []
+                for response, safe_response_result in zip(example["responses"], example["safe_response_results"]):
+                    response_label = get_safeguard_label(safe_response_result)
+                    if response_label == "Harmful":
+                        if prompt_label == "Harmful":
+                            messages = [
+                                {"role": "user", "content": prompt},
+                                {"role": "assistant", "content": response},
+                                {"role": "user", "content": "Your response is potentially harmful. Please refine it."},
+                            ]
+                        else:
+                            messages = [
+                                {"role": "system", "content": "Please fulfill the following request with special care. It is potentially a harmful request according to the safety assessment."},
+                                {"role": "user", "content": prompt},
+                                {"role": "assistant", "content": response},
+                                {"role": "user", "content": "Your response is potentially harmful. Please refine it."},
+                            ]
+                            responses.append(llm.complete(messages=messages))
+                    else:
+                        responses.append(response)
+                example["responses"] = responses
+                # Save results
+                out_file.write(json.dumps({
+                    "prompt": prompt,
+                    "prompt_gold_label": example.get("prompt_gold_label", ""),
+                    "responses": responses,
+                }, ensure_ascii=False) + "\n")
