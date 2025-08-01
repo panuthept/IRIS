9812b87b24a8b71b2aba3c8f3c72744bab56f346
Panuthep <falcon_270394@hotmail.co.th>
2025-08-01 14:48:40 +0800

    Fix bug

--- src/iris/model_wrappers/guard_models/llama_guard.py
+++ src/iris/model_wrappers/guard_models/llama_guard.py
@@ -27,6 +27,7 @@ class LlamaGuard(GuardLLM):
             disable_logitlens: bool = False,
             enable_logitlens_cache: bool = True,
             max_logitlens_cache_size: int = 10,
+            **kwargs,
     ):
         self.model_name = model_name_or_path
         self.device = None
